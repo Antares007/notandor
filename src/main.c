4253ae1b1a96e3131f238ad8515991cf862d81a2
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-10-12 13:07:41 +0400

    def p1..4 for parameters

--- src/main.c
+++ src/main.c
@@ -5,32 +5,31 @@ N(ano);
 N(noa);
 N(oan);
 N(ψ);
-
-N(one ) { A(1, ano, 020, ψ); }
-N(add_) { R(Q_t, r); R(Q_t, l); A(l + r, ano); }
-N(add ) { A(add_, 010, ψ); }
+N(p1) { A(ano, 020, ψ); }
+N(p2) { A(ano, 030, ψ); }
+N(p3) { A(ano, 040, ψ); }
+N(p4) { A(ano, 050, ψ); }
+N(one) { A(1, ano, 020, ψ); }
+N(add_) {
+  R(Q_t, r);
+  R(Q_t, l);
+  A(l + r, ano);
+}
+N(add) { A(add_, 010, ψ); }
 
 N(o_ray) { printf("OOR\n"); }
 N(a_ray) { printf("AND %lu\n", ο[--ν].Q); }
 N(n_ray) { printf("NOT\n"); }
 
-N(pano) { printf("1\n"); ano(C); }
-N(poan) { printf("0\n"); oan(C); }
-N(pnoa) { printf("2\n"); noa(C); }
-N(and) { }
+N(pano) { printf("1\n"), ano(C); }
+N(poan) { printf("0\n"), oan(C); }
+N(pnoa) { printf("2\n"), noa(C); }
+N(and) {}
 N(ATree) {
-  A(
-    ano,
-      poan, ano, 0001, ψ,
-    040, ψ,
-      pano, ano, 0010, ψ,
-    040, ψ,
-      pnoa, ano, 0100, ψ,
-    040, ψ);
+  A(ano, poan, ano, 0001, ψ, 040, ψ, pano, ano, 0010, ψ, 040, ψ, pnoa, ano,
+    0100, ψ, 040, ψ);
 }
-N(ST) {
-  A(ano, one, one, add, one, add, one, add);
-}
+N(ST) { A(ano, one, 2, p1, add, one, add, one, add); }
 int main() {
   p_t ο[512];
   Q_t α = sizeof(ο) / sizeof(*ο);
