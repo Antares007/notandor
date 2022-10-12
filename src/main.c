e765ccc6a122d6f33ba7a392c927bc83a60b5fe4
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-10-12 17:09:26 +0400

    prep for vbar split

--- src/main.c
+++ src/main.c
@@ -9,13 +9,14 @@ N(p1) { A(ano, 020, ψ); }
 N(p2) { A(ano, 030, ψ); }
 N(p3) { A(ano, 040, ψ); }
 N(p4) { A(ano, 050, ψ); }
-N(one) { A(1, ano, 020, ψ); }
-N(add_) {
+N(oneβ) { A(1, ano); }
+N(one) { A(oneβ, 010, ψ); }
+N(addβ) {
   R(Q_t, r);
   R(Q_t, l);
   A(l + r, ano);
 }
-N(add) { A(add_, 010, ψ); }
+N(add) { A(addβ, 010, ψ); }
 
 N(o_ray) { printf("OOR\n"); }
 N(a_ray) { printf("AND %lu\n", ο[--ν].Q); }
@@ -24,10 +25,17 @@ N(n_ray) { printf("NOT\n"); }
 N(pano) { printf("1\n"), ano(C); }
 N(poan) { printf("0\n"), oan(C); }
 N(pnoa) { printf("2\n"), noa(C); }
-N(and) {}
-N(ATree) {
-  A(ano, poan, ano, 0001, ψ, 040, ψ, pano, ano, 0010, ψ, 040, ψ, pnoa, ano,
-    0100, ψ, 040, ψ);
+void and(p_t *ο, Q_t α, Q_t ν) {
+  // p_t nο[0x1000];
+  // code, code, code, code, ..., code.
+  
+}
+void ATree(p_t *ο, Q_t α, Q_t ν) {
+  A(
+                   ano,
+    one, one, add, and,
+    one, one, add
+  );
 }
 N(ST) { A(ano, one, 2, p1, add, one, add, one, add); }
 int main() {
@@ -38,6 +46,6 @@ int main() {
   ο[--α].c = a_ray;
   ο[--α].c = o_ray;
   ο[--α].Q = 0111;
-  ST(C);
+  A(ST);
   return 5;
 }
