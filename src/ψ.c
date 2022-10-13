1aaa4bf7acd81358412ab7dcc96e1787ecbe1b19
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-10-14 02:01:08 +0400

    can find lrec

--- src/ψ.c
+++ src/ψ.c
@@ -1,20 +1,21 @@
 #include "ψ.h"
 #include "ψ_logn.h"
 // clang-format off
-N(noa) {
+n_t lr[1024]; Q_t lr_len = 0;
+N(noa) { lr_len = 0;
   Q_t g = ο[α++].Q, nc = g >> 6 & 7, ac = g >> 3 & 7, oc = g >> 0 & 7;
   for (Q_t i = 0; i < nc; i++) ο[ν++].v = ο[α++].v;
   α += ac + oc;
   nc ? Ο : noa(Τ);
 }
-N(ano) {
+N(ano) { lr_len = 0;
   Q_t g = ο[α++].Q, nc = g >> 6 & 7, ac = g >> 3 & 7, oc = g >> 0 & 7;
   α += nc;
   for (Q_t i = 0; i < ac; i++) ο[ν++].v = ο[α++].v;
   α += oc;
   ac ? Ο : ano(Τ);
 }
-N(oan) {
+N(oan) { lr_len = 0;
   Q_t g = ο[α++].Q, nc = g >> 6 & 7, ac = g >> 3 & 7, oc = g >> 0 & 7;
   α += nc + ac;
   for (Q_t i = 0; i < oc; i++) ο[ν++].v = ο[α++].v;
@@ -24,5 +25,9 @@ N(ψ) {
   Q_t g = ο[--ν].Q, nc = g >> 6 & 7, ac = g >> 3 & 7, oc = g >> 0 & 7;
   for (Q_t i = 0; i < nc + ac + oc; i++) ο[--α].v = ο[--ν].v;
   ο[--α].Q = g;
-  Ο;
+  R(n_t, nar);
+  for(Q_t i = 0; i < lr_len; i++)
+    if (lr[i] == nar) return (printf("LREC!\n"), (void) 0);
+  lr[lr_len++] = nar;
+  nar(Τ);
 }
