a4c210f6f2748ba8bb3419485cf2ee30465d378f
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-10-21 13:05:38 +0400

    s8

--- src/main12.c
+++ src/main12.c
@@ -1,76 +1,85 @@
 #include "oars.h"
 // clang-format off
 N(Cε); N(Cβ); N(Cα); N(ψ);
-#define BT ο₃[α₃++].Q = ρ,        ο₃[α₃++].Q = α,        ο₃[α₃++].v = ο,       \
-           ο          = ο₃,       α          = α₃,       ρ          = ρ₃,      \
-           ο₃         = ο₂,       α₃         = α₂,       ρ₃         = ρ₂,      \
-           ο₂         = ο₁,       α₂         = α₁,       ρ₂         = ρ₁,      \
-           ο₁         = ο[--α].v, α₁         = ο[--α].Q, ρ₁         = ο[--α].Q,
 N(αο  ) { LOG; }
-#include<unistd.h>
-N(βο  ) { LOG; ρ -= 4; BT (void)0; if (ο[α-1].v == Cβ || ο[α-1].v == ψ) O; }
-N(εο  ) { LOG; }
-N(p_  ) {      Q_t v = ο[--α].Q; printf("%lu\n", v); A(Cβ) O; }
+//#include<unistd.h>
+N(βο  ) { LOG,ρ[σ]-=4,SCCW ((ο[σ][α[σ]-1].v == Cβ || ο[σ][α[σ]-1].v == ψ)?O:(void)0); }
+N(ωο  ) { LOG; }
+N(p_  ) {      Q_t v = ο[σ][--α[σ]].Q; printf("%lu\n", v); A(Cβ) O; }
 N(p   ) {      A(p_, 020, ψ) O; }
 N(p1  ) { LOG; A(p_, 010, ψ) O; }
 
-N(_bechd_ ) { printf("%s\n", ο[--α].cs), A(Cβ) O; }
-N(dabechde) { A(_bechd_, 010, ψ) O; }
-
 N(one_    ) { LOG; A(1, Cβ) O; }
 N(one     ) { LOG; A(one_, 010, ψ) O; }
-N(add_    ) { LOG; Q_t r = ο[--α].Q; Q_t l = ο[--α].Q; A(l+r, Cβ) O; }
+
+N(two_    ) { LOG; A(2, Cβ) O; }
+N(two     ) { LOG; A(two_, 010, ψ) O; }
+
+N(add_    ) { LOG; Q_t r = ο[σ][--α[σ]].Q; Q_t l = ο[σ][--α[σ]].Q; A(l+r, Cβ) O; }
 N(add     ) { LOG; A(add_, 010, ψ) O;}
 
 N(mamaφ   ) { LOG; A("mama", Cβ) O; }
-N(mamam_3 ) { LOG; BT BT BT A(mamaφ, 010, ψ) BT O; }
+N(mamam_3 ) { LOG; SCCW A(mamaφ, 010, ψ) SCW O; }
 
 N(shvilφ  ) { LOG; A("shvil", Cβ) O; }
-N(shvils_3) { LOG; BT BT BT A(shvilφ, 010, ψ) BT O; }
+N(shvils_3) { LOG; SCCW A(shvilφ, 010, ψ) SCW O; }
 
 N(sakhlφ  ) { LOG; A("sakhl", Cβ) O; }
-N(sakhli_1) { LOG; BT A(sakhlφ, 010, ψ) BT BT BT O;}
+N(sakhli_1) { LOG; SCW A(sakhlφ, 010, ψ) SCCW O;}
 
 N(shenφ   ) { LOG;
-  const char *S  = ο[--α].cs;
-  const char *Op = ο₁[--α₁].cs;
-  const char *Oi = ο₂[--α₂].cs;
-  printf("%s %s %s shena\n", S, Op, Oi);
-  A(Cβ) O;
+  //const char *S  = ο[--α].cs;
+  //const char *Op = ο₁[--α₁].cs;
+  //const char *Oi = ο₂[--α₂].cs;
+  //printf("%s %s %s shena\n", S, Op, Oi);
+  //A(Cβ) O;
 }
-N(au_shen_a_331   ) {LOG; } // man mas is
-N(ami_shen_a_311  ) {LOG; } // man me is
-N(avu_shen_e_131  ) {LOG; } // me mas is
-N(agi_shen_e_121  ) {LOG; } // me shen is
+N(au_shen_a_331   ) {LOG; A(3,3,1,shenφ, 040, ψ) O;
+} // man mas is
+//N(ami_shen_a_311  ) {LOG; } // man me is
+//N(avu_shen_e_131  ) {LOG; } // me mas is
+//N(agi_shen_e_121  ) {LOG; } // me shen is
 // I   მე და ჩვენ
 // II  შენ და თქვენ
 // III ის (იგი), ისინი, მან, მას (იმას, ამას), მათ
 N(sitkva ) { LOG;
-  //A(Cβ, mamam_3, shvils_3, sakhli_1, au_shen_a_331) O;
-  A(Cβ, one, one, add, one, add, one, add, one, add, one, add, one, add, p1) O;
+  A(Cβ, mamam_3, shvils_3, sakhli_1, au_shen_a_331) O;
+  //A(Cβ,one,one,add,one,add,one,add,one,add,one,add,one,add,p1) O;
 }
-N(test);
+N(test1);
+N(test2);
 #define Σ 512
 int main() {
-  s_t *ο  = (s_t[Σ]){}; Q_t α  = 0, ρ  = Σ;
-  s_t *ο₁ = (s_t[Σ]){}; Q_t α₁ = 0, ρ₁ = Σ;
-  s_t *ο₂ = (s_t[Σ]){}; Q_t α₂ = 0, ρ₂ = Σ;
-  s_t *ο₃ = (s_t[Σ]){}; Q_t α₃ = 0, ρ₃ = Σ;
-  for(Q_t i = 0; i < 4; i++) {
-    LOG, ο[--ρ].c = αο, ο[--ρ].c = βο, ο[--ρ].c = εο, ο[--ρ].Q = 0111;
-    BT (void)0;
-  }
+  s_t *ο[SC] = {(s_t[Σ]){}, (s_t[Σ]){}, (s_t[Σ]){}, (s_t[Σ]){},
+                (s_t[Σ]){}, (s_t[Σ]){}, (s_t[Σ]){}, (s_t[Σ]){}};
+  Q_t  α[SC],
+       ρ[SC],
+       σ = 0;
+  for(Q_t i = 0; i < SC; i++) 
+    α[i] = 0,
+    ρ[i] = 150,
+    ο[i][--ρ[i]].c = αο,
+    ο[i][--ρ[i]].c = βο,
+    ο[i][--ρ[i]].c = ωο,
+    ο[i][--ρ[i]].Q = 0111;
   A(sitkva) O;
-  //  α--,ο[α].c(Τ);
-  //  A(Cβ, test, 010, ψ) O;
-  return 7;
+  //test1(ο,α,ρ,σ);
+  //test2(ο,α,ρ,σ);
+  return 0;
+
 }
 #include <assert.h>
-N(test) { LOG; 
-  A(0) BT A(3) BT A(2) BT A(1) BT (void)0;
-  assert(ο [--α].Q == 0);
-  assert(ο₁[--α₁].Q == 1);
-  assert(ο₂[--α₂].Q == 2);
-  assert(ο₃[--α₃].Q == 3);
-  A(Cβ) O;
+N(test1) { LOG; 
+  A(0) SCW A(1) SCW A(2) SCW A(3) SCW (void)0;
+  assert(ο[0][--α[0]].Q == 0);
+  assert(ο[1][--α[1]].Q == 1);
+  assert(ο[2][--α[2]].Q == 2);
+  assert(ο[3][--α[3]].Q == 3);
+}
+N(test2) { LOG; 
+  A(-1) SCCW A(3) SCCW A(2) SCCW A(1) SCCW (void)0;
+  assert(ο[0][--α[0]].Q == -1);
+  assert(ο[SC-3][--α[SC-3]].Q == 1);
+  assert(ο[SC-2][--α[SC-2]].Q == 2);
+  assert(ο[SC-1][--α[SC-1]].Q == 3);
 }
--- src/oars.h
+++ src/oars.h
@@ -9,16 +9,20 @@ typedef struct s_t {
     void (*c)();
   };
 } s_t;
-#define N(name) void name(s_t *ο, Q_t α, Q_t ρ,  \
-                          s_t *ο₁, Q_t α₁, Q_t ρ₁,  \
-                          s_t *ο₂, Q_t α₂, Q_t ρ₂,  \
-                          s_t *ο₃, Q_t α₃, Q_t ρ₃)
-#define A1(b) ο[α++].v = (void*)b,
+#define N(name) void name(s_t **ο, Q_t *α, Q_t *ρ, Q_t σ)
+#define A1(b) ο[σ][α[σ]++].v = (void*)b,
 #include "evalmap.h"
 #define A(...) EVAL(MAP(A1, __VA_ARGS__)) 
-#define Τ ο, α, ρ, ο₁, α₁, ρ₁, ο₂, α₂, ρ₂, ο₃, α₃, ρ₃
-#define O α--, ο[α].c(Τ)
+#define O α[σ]--, ο[σ][α[σ]].c(ο, α, ρ, σ)
+#define SC 8
+#define SCW (σ=(σ+1)%SC),
+#define SCCW (σ=(σ+SC-1)%SC),
 // ψ α β ε
 #include <stdio.h>
 
-#define LOG printf("%lx %02lu %lu %s\n", ((Q_t)ο >> (Q_t)12) & 0xfff, α, ρ, __FUNCTION__)
+#define LOG printf("%lx %02lu %lu %lu %s\n",  \
+    ((Q_t)ο[σ] >> (Q_t)12) & 0xfff,           \
+    α[σ],                                     \
+    ρ[σ],                                     \
+    σ,                                        \
+    __FUNCTION__)
