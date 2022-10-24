deb55653183710a3d3892ebd104b1c2a2009ab07
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-10-24 21:49:27 +0400

    cant count upto seven

--- src/main48.c
+++ src/main48.c
@@ -1,5 +1,6 @@
 // clang-format off
 typedef unsigned long Q_t;
+typedef   signed long q_t;
 typedef struct s_t {
   union {
     Q_t Q;
@@ -8,12 +9,37 @@ typedef struct s_t {
     void(*c)(struct s_t*ο, Q_t α, Q_t ρ, struct s_t*σ);
   };
 } s_t;
-#define Ai(i, ...)                                \
-  {                                               \
-    void*s[]={__VA_ARGS__};                       \
-    for(Q_t ξ=0; ξ < sizeof(s)/sizeof(*s); ξ++)   \
-      ; \
-  }}
+#define Ai(i, ...)                                  \
+  {                                                 \
+    void*s[]={__VA_ARGS__};                         \
+    s_t *iο = ο[5+i].s;                             \
+    s_t *iσ = iο[3].s;                              \
+    Q_t  iα = iο[1].Q;                              \
+    for(Q_t ξ=0; ξ < sizeof(s) / sizeof(*s); ξ++)   \
+      iσ[iα++].v = s[ξ];                            \
+    iο[1].Q = iα;                                   \
+  }
+#define Ri(i, ...)                                  \
+  {                                                 \
+    void*s[]={__VA_ARGS__};                         \
+    s_t *iο = ο[5+i].s;                             \
+    Q_t  iρ = iο[2].Q;                              \
+    for(q_t ξ=sizeof(s) / sizeof(*s); ξ > -1 ; --ξ) \
+      iο[--iρ].v = s[ξ];                            \
+    iο[2].Q = iρ;                                   \
+  }
+#define A(...)                                      \
+  {                                                 \
+    void*s[]={__VA_ARGS__};                         \
+    for(Q_t ξ=0; ξ < sizeof(s) / sizeof(*s); ξ++)   \
+      σ[α++].v = s[ξ];                              \
+  }
+#define R(...)                                      \
+  {                                                 \
+    void*s[]={__VA_ARGS__};                         \
+    for(q_t ξ=sizeof(s) / sizeof(*s)-1; ξ > -1 ; ξ--) \
+      ο[--ρ].v = s[ξ];                              \
+  }
 #define N(name) void name(s_t*ο, Q_t α, Q_t ρ, s_t*σ)
 #define G       printf("%lu %02lu %03lu %s\n", ο[4].Q, α, ρ, __FUNCTION__);
 #define O       σ[α-1].c(ο, α-1, ρ, σ)
@@ -28,10 +54,23 @@ N(God     ) {G;
   ρ += oorc;
   andc ? O : God(ο, α, ρ, σ);
 }
+#include <unistd.h>
 N(ray_oor ) {G; }
-N(ray_and ) {G; ο[1].Q = α, God(ο[0].s, ο[0].s[1].Q, ο[0].s[2].Q, ο[0].s[3].s); }
+N(ray_and ) {G; while (α > 1) --α, printf("%lu ", σ[--α].Q); printf("\n");
+                sleep(1), ο[1].Q = α, ο[2].Q = ρ - 4, God(ο[0].s, ο[0].s[1].Q, ο[0].s[2].Q, ο[0].s[3].s); }
 N(ray_not ) {G; }
 N(B       ) {G; God(ο, α+1, ρ, σ); }
+N(T) {
+  Ri(0, 3,4,6) O;
+}
+N(one   ) { G;  R(020, 1, B) O; }
+N(add   ) { G;  Q_t r = (--α, σ[--α].Q);
+                Q_t l = (--α, σ[--α].Q);
+                R(020, (l + r), B) O; }
+N(two   ) { G;  R(010, one,   010, one,   010, add) O; }
+N(three ) { G;  R(010, one,   010, two,   010, add) O; }
+N(four  ) { G;  R(010, one,   010, three, 010, add) O; }
+N(seven ) { G;  R(010, three, 010, four,  010, add) O; }
 int main( ) {
   s_t ο[8][Σ];
   s_t σ[4][Σ];
@@ -49,9 +88,17 @@ int main( ) {
     ο[i][1].Q   = 1,
     ο[i][2].Q   = Σ-4,
     ο[i][4].Q   = i,
+    ο[i][5+0].v= ο[0],
+    ο[i][5+1].v= ο[1],
+    ο[i][5+2].v= ο[2],
+    ο[i][5+3].v= ο[3],
+    ο[i][5+4].v= ο[4],
+    ο[i][5+5].v= ο[5],
+    ο[i][5+6].v= ο[6],
+    ο[i][5+7].v= ο[7],
     ο[i][Σ-1].v = ray_oor,
     ο[i][Σ-2].v = ray_and,
     ο[i][Σ-3].v = ray_not,
     ο[i][Σ-4].v = 0111;
-  Nain(ο[0], 1, Σ-4, σ[0]);
+  seven(ο[0], 1, Σ-4, σ[0]);
 }
