9f7cf5e581269947e4c9073be094653f4706129a
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-10-26 05:05:57 +0400

    Advance&Retrieve Put&Take

--- src/main48-12.c
+++ src/main48-12.c
@@ -9,48 +9,52 @@ typedef struct s_t { union {
 #define οαρσΜΙΣ         ο,    α,    ρ,            σ,    Μ,    Ι,    Σ
 #define οαρσΜ           ο,    α,    ρ,            σ,    Μ
 #define L printf("%lu %02lu %03lu %s\n", Ι, α[Μ[Ι]], ρ[Ι], __FUNCTION__);
-#define R(i, ...) { void*s[]={__VA_ARGS__};                   \
+
+#define A(Ι, ...) { void*s[]={__VA_ARGS__};                   \
+                    for(Q_t ξ=0; ξ<sizeof(s)/sizeof(*s);ξ++)  \
+                      σ[Μ[Ι]][α[Μ[Ι]]++].v = s[ξ]; }
+#define R(Ι)      σ[Μ[Ι]][--α[Μ[Ι]]]
+#define P(Ι, ...) { void*s[]={__VA_ARGS__};                   \
                     Q_t ξ=sizeof(s)/sizeof(*s);               \
                     while(ξ)                                  \
-                      ο[i][--ρ[i]].v = s[--ξ]; }
-#define A(i, ...) { void*s[]={__VA_ARGS__};                   \
-                    for(Q_t ξ=0; ξ<sizeof(s)/sizeof(*s);ξ++)  \
-                      σ[Μ[i]][α[Μ[i]]++].v = s[ξ]; }
+                      ο[Ι][--ρ[Ι]].v = s[--ξ]; }
+#define T(Ι)      ο[Ι][ρ[Ι]++]
 #include<stdio.h>
-void Got      (OARS) { ;Q_t ψ = ο[Ι][ρ[Ι]++].Q, nc = ψ >> 6 & 7, ac = ψ >> 3 & 7, oc = ψ & 7;
-                        for (Q_t i = 0; i < nc; i++) σ[Μ[Ι]][α[Μ[Ι]]++].v = ο[Ι][ρ[Ι]++].v;
+#include<assert.h>
+void Got      (OARS) { ;Q_t ψ = T(Ι).Q, nc = ψ >> 6 & 7, ac = ψ >> 3 & 7, oc = ψ & 7;
+                        for (Q_t i = 0; i < ac; i++) A(Ι, T(Ι).v);
                         ρ[Ι] += ac + oc;
-                        nc ? σ[Μ[Ι]][--α[Μ[Ι]]].c(οαρσΜΙΣ) : Got(οαρσΜΙΣ); }
+                        nc ? R(Ι).c(οαρσΜΙΣ) : Got(οαρσΜΙΣ); }
 void God      (OARS) { ;Q_t ψ = ο[Ι][ρ[Ι]++].Q, nc = ψ >> 6 & 7, ac = ψ >> 3 & 7, oc = ψ & 7;
                         ρ[Ι] += nc;
-                        for (Q_t i = 0; i < ac; i++) σ[Μ[Ι]][α[Μ[Ι]]++].v = ο[Ι][ρ[Ι]++].v;
+                        for (Q_t i = 0; i < ac; i++) A(Ι, T(Ι).v);
                         ρ[Ι] += oc;
-                        ac ? σ[Μ[Ι]][--α[Μ[Ι]]].c(οαρσΜΙΣ) : God(οαρσΜΙΣ); }
+                        ac ? R(Ι).c(οαρσΜΙΣ) : God(οαρσΜΙΣ); }
 void Gor      (OARS) { ;Q_t ψ = ο[Ι][ρ[Ι]++].Q, nc = ψ >> 6 & 7, ac = ψ >> 3 & 7, oc = ψ & 7;
                         ρ[Ι] += nc + ac;
-                        for (Q_t i = 0; i < oc; i++) σ[Μ[Ι]][α[Μ[Ι]]++].v = ο[Ι][ρ[Ι]++].v;
-                        oc ? σ[Μ[Ι]][--α[Μ[Ι]]].c(οαρσΜΙΣ) : Gor(οαρσΜΙΣ); }
-void B        (OARS) { ;α[Μ[Ι]]++, God(οαρσΜ,7,Σ); }
-void C        (OARS) { ;α[Μ[Ι]]++, God(οαρσΜΙΣ); }
+                        for (Q_t i = 0; i < ac; i++) A(Ι, T(Ι).v);
+                        oc ? R(Ι).c(οαρσΜΙΣ) : Gor(οαρσΜΙΣ); }
+void B        (OARS) { ;A(Ι, B) God(οαρσΜ,7,Σ); }
+void C        (OARS) { ;A(Ι, C) God(οαρσΜΙΣ); }
 void ray_God  (OARS) { ;if (Ι) God(οαρσΜ,(Ι+7)%8,Σ); }
-void dot      (OARS) { ;σ[Μ[Ι]][--α[Μ[Ι]]].c(οαρσΜΙΣ); }
+
 void mama_    (OARS) {L;A(Ι, "მამა", C)   God(οαρσΜΙΣ); }
-void mamam    (OARS) {L;R(2, 010, mama_)  dot(οαρσΜΙΣ); }
+void mamam    (OARS) {L;P(2, 010, mama_)  R(Ι).c(οαρσΜΙΣ); }
 void shvil_   (OARS) {L;A(Ι, "შვილ", C)   God(οαρσΜΙΣ); }
-void shvils   (OARS) {L;R(3, 010, shvil_) dot(οαρσΜΙΣ); }
+void shvils   (OARS) {L;P(3, 010, shvil_) R(Ι).c(οαρσΜΙΣ); }
 void sakhl_   (OARS) {L;A(Ι, "სახლ", C)   God(οαρσΜΙΣ); }
-void sakhli   (OARS) {L;R(1, 010, sakhl_) dot(οαρσΜΙΣ); }
-void _shen_   (OARS) {L;Q_t pi3 = σ[Μ[Ι]][--α[Μ[Ι]]].Q;
-                        Q_t pi2 = σ[Μ[Ι]][--α[Μ[Ι]]].Q;
-                        Q_t pi1 = σ[Μ[Ι]][--α[Μ[Ι]]].Q;
+void sakhli   (OARS) {L;P(1, 010, sakhl_) R(Ι).c(οαρσΜΙΣ); }
+void _shen_   (OARS) {L;Q_t pi3 = R(Ι).Q;
+                        Q_t pi2 = R(Ι).Q;
+                        Q_t pi1 = R(Ι).Q;
                         printf("%lu %lu %lu\n", pi1, pi2, pi3);
-                        const char* ps3 = (--α[Μ[pi3]], σ[Μ[pi3]][--α[Μ[pi3]]].Q);
-                        const char* ps2 = (--α[Μ[pi2]], σ[Μ[pi2]][--α[Μ[pi2]]].Q);
-                        const char* ps1 = (--α[Μ[pi1]], σ[Μ[pi1]][--α[Μ[pi1]]].Q);
+                        const char* ps3 = (assert(R(pi3).v==C), R(pi3).Q);
+                        const char* ps2 = (assert(R(pi2).v==C), R(pi2).Q);
+                        const char* ps1 = (assert(R(pi1).v==C), R(pi1).Q);
                         printf("%s  %s  %s შენ.\n", ps1, ps2, ps3);
                         God(οαρσΜΙΣ); }
-void aushena  (OARS) {L;R(Ι, 040, 2, 2, 1, _shen_) dot(οαρσΜΙΣ); }
-void ntext    (OARS) {L;A(Ι, shvils, mamam, aushena, sakhli) dot(οαρσΜΙΣ); }
+void aushena  (OARS) {L;P(Ι, 040, 2, 2, 1, _shen_) R(Ι).c(οαρσΜΙΣ); }
+void ntext    (OARS) {L;A(Ι, shvils, mamam, aushena, sakhli) R(Ι).c(οαρσΜΙΣ); }
 int main() {
   Q_t Σ;
   s_t b[12][Σ = 512];
