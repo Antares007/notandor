0d02e812d96ab45d31bbd21a69a71a5faea3fd38
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-10-26 06:22:42 +0400

    da dedam mat namckhvari gamouckho

--- src/main48-12.c
+++ src/main48-12.c
@@ -21,41 +21,56 @@ typedef struct s_t { union {
 #define T(Ι)      ο[Ι][ρ[Ι]++]
 #include<stdio.h>
 #include<assert.h>
-void go2      (OARS) { ;Q_t ψ = T(Ι).Q, nc = ψ >> 6 & 7, ac = ψ >> 3 & 7, oc = ψ & 7;
-                        for (Q_t i = 0; i < ac; i++) A(Ι, T(Ι).v);
-                        ρ[Ι] += ac + oc;
-                        nc ? R(Ι).c(οαρσΜΙΣ) : go2(οαρσΜΙΣ); }
-void go1      (OARS) { ;Q_t ψ = ο[Ι][ρ[Ι]++].Q, nc = ψ >> 6 & 7, ac = ψ >> 3 & 7, oc = ψ & 7;
-                        ρ[Ι] += nc;
-                        for (Q_t i = 0; i < ac; i++) A(Ι, T(Ι).v);
-                        ρ[Ι] += oc;
-                        ac ? R(Ι).c(οαρσΜΙΣ) : go1(οαρσΜΙΣ); }
-void go0      (OARS) { ;Q_t ψ = ο[Ι][ρ[Ι]++].Q, nc = ψ >> 6 & 7, ac = ψ >> 3 & 7, oc = ψ & 7;
-                        ρ[Ι] += nc + ac;
-                        for (Q_t i = 0; i < ac; i++) A(Ι, T(Ι).v);
-                        oc ? R(Ι).c(οαρσΜΙΣ) : go0(οαρσΜΙΣ); }
-void B        (OARS) { ;A(Ι, B) go1(οαρσΜ,7,Σ); }
-void C        (OARS) { ;A(Ι, C) go1(οαρσΜΙΣ); }
-void end      (OARS) { ; }
-void next1    (OARS) {L;go1(οαρσΜ,(Ι+7)%8,Σ); }
+void go2        (OARS) { ;Q_t ψ = T(Ι).Q, nc = ψ >> 6 & 7, ac = ψ >> 3 & 7, oc = ψ & 7;
+                          for (Q_t i = 0; i < ac; i++) A(Ι, T(Ι).v);
+                          ρ[Ι] += ac + oc;
+                          nc ? R(Ι).c(οαρσΜΙΣ) : go2(οαρσΜΙΣ); }
+void go1        (OARS) { ;Q_t ψ = ο[Ι][ρ[Ι]++].Q, nc = ψ >> 6 & 7, ac = ψ >> 3 & 7, oc = ψ & 7;
+                          ρ[Ι] += nc;
+                          for (Q_t i = 0; i < ac; i++) A(Ι, T(Ι).v);
+                          ρ[Ι] += oc;
+                          ac ? R(Ι).c(οαρσΜΙΣ) : go1(οαρσΜΙΣ); }
+void go0        (OARS) { ;Q_t ψ = ο[Ι][ρ[Ι]++].Q, nc = ψ >> 6 & 7, ac = ψ >> 3 & 7, oc = ψ & 7;
+                          ρ[Ι] += nc + ac;
+                          for (Q_t i = 0; i < ac; i++) A(Ι, T(Ι).v);
+                          oc ? R(Ι).c(οαρσΜΙΣ) : go0(οαρσΜΙΣ); }
+void B          (OARS) { ;A(Ι, B) go1(οαρσΜ,7,Σ); }
+void C          (OARS) { ;A(Ι, C) go1(οαρσΜΙΣ); }
+void end        (OARS) { ; }
+void next1      (OARS) {L;go1(οαρσΜ,(Ι+7)%8,Σ); }
 
-void mama_    (OARS) {L;A(Ι, "მამა", C)   go1(οαρσΜΙΣ); }
-void mamam    (OARS) {L;P(2, 010, mama_)  R(Ι).c(οαρσΜΙΣ); }
-void shvil_   (OARS) {L;A(Ι, "შვილ", C)   go1(οαρσΜΙΣ); }
-void shvils   (OARS) {L;P(3, 010, shvil_) R(Ι).c(οαρσΜΙΣ); }
-void sakhl_   (OARS) {L;A(Ι, "სახლ", C)   go1(οαρσΜΙΣ); }
-void sakhli   (OARS) {L;P(1, 010, sakhl_) R(Ι).c(οαρσΜΙΣ); }
-void _shen_   (OARS) {L;Q_t pi3 = R(Ι).Q;
-                        Q_t pi2 = R(Ι).Q;
-                        Q_t pi1 = R(Ι).Q;
-                        printf("%lu %lu %lu\n", pi1, pi2, pi3);
-                        const char* ps3 = (assert(R(pi3).v==C), R(pi3).Q);
-                        const char* ps2 = (assert(R(pi2).v==C), R(pi2).Q);
-                        const char* ps1 = (assert(R(pi1).v==C), R(pi1).Q);
-                        printf("%s  %s  %s შენ.\n", ps1, ps2, ps3);
-                        go1(οαρσΜΙΣ); }
-void aushena  (OARS) {L;P(Ι, 040, 2, 2, 1, _shen_) R(Ι).c(οαρσΜΙΣ); }
-void ntext    (OARS) {L;A(Ι, shvils, mamam, aushena, sakhli) R(Ι).c(οαρσΜΙΣ); }
+void mama_      (OARS) {L;A(Ι, "მამა", C)   go1(οαρσΜΙΣ); }
+void mamam      (OARS) {L;P(2, 010, mama_)  R(Ι).c(οαρσΜΙΣ); }
+void shvil_     (OARS) {L;A(Ι, "შვილ", C)   go1(οαρσΜΙΣ); }
+void shvils     (OARS) {L;P(3, 010, shvil_) R(Ι).c(οαρσΜΙΣ); }
+void sakhl_     (OARS) {L;A(Ι, "სახლ", C)   go1(οαρσΜΙΣ); }
+void sakhli     (OARS) {L;P(1, 010, sakhl_) R(Ι).c(οαρσΜΙΣ); }
+void _shen_     (OARS) {L;Q_t pi3 = R(Ι).Q;
+                          Q_t pi2 = R(Ι).Q;
+                          Q_t pi1 = R(Ι).Q;
+                          printf("%lu %lu %lu\n", pi1, pi2, pi3);
+                          const char* ps3 = (assert(R(pi3).v==C), R(pi3).Q);
+                          const char* ps2 = (assert(R(pi2).v==C), R(pi2).Q);
+                          const char* ps1 = (assert(R(pi1).v==C), R(pi1).Q);
+                          printf("%s  %s  %s.\n", ps1, ps2, ps3);
+                          go1(οαρσΜΙΣ); }
+void aushena    (OARS) {L;P(Ι, 040, 2, 2, 1, _shen_)  R(Ι).c(οαρσΜΙΣ); }
+
+void da         (OARS) {L;for(Q_t i=0;i<8;i++)P(i, 0111, end, next1, end) R(Ι).c(οαρσΜΙΣ); }
+
+void deda_      (OARS) {L;A(Ι, "დედა", C)       go1(οαρσΜΙΣ); }
+void dedam      (OARS) {L;P(2, 010, deda_)      R(Ι).c(οαρσΜΙΣ); }
+void ma_        (OARS) {L;A(Ι, "მათ", C)        go1(οαρσΜΙΣ); }
+void mat        (OARS) {L;P(2, 010, ma_)        R(Ι).c(οαρσΜΙΣ); }
+void namckhvar_ (OARS) {L;A(Ι, "ნამცხვარ", C)   go1(οαρσΜΙΣ); }
+void namckhvari (OARS) {L;P(2, 010, namckhvar_) R(Ι).c(οαρσΜΙΣ); }
+void _ckho_     (OARS) {L;                      _shen_(οαρσΜΙΣ); }
+void gamouckho  (OARS) {L;P(Ι, 040, 2,2,2, _ckho_)     R(Ι).c(οαρσΜΙΣ); }
+
+
+void ntext      (OARS) {L;A(Ι,  mamam, sakhli, shvils, aushena,
+                                da, dedam, mat, namckhvari, gamouckho)
+                                R(Ι).c(οαρσΜΙΣ); }
 int main() {
   Q_t Σ;
   s_t b[12][Σ = 512];
