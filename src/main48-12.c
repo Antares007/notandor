9c389d6958c3a3f649c5f48e79b713dfe4227138
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-10-27 10:41:23 +0400

    ο→ω

--- src/main48-12.c
+++ src/main48-12.c
@@ -3,11 +3,11 @@ typedef unsigned long Q_t;
 typedef struct s_t { union {
   Q_t Q;
   void *v;
-  void (*c)(struct s_t**ο,Q_t*α,Q_t*ρ,struct s_t**σ,Q_t*Μ,Q_t Σ,Q_t Ι);
+  void (*c)(struct s_t**ω,Q_t*α,Q_t*ρ,struct s_t**σ,Q_t*Μ,Q_t Σ,Q_t Ι);
 }; } s_t;                                                      
-#define OARS       s_t**ο,Q_t*α,Q_t*ρ,       s_t**σ,Q_t*Μ,Q_t Σ,Q_t Ι
-#define οαρσΜΣΙ         ο,    α,    ρ,            σ,    Μ,    Σ,    Ι
-#define οαρσΜΣ          ο,    α,    ρ,            σ,    Μ,    Σ
+#define OARS       s_t**ω,Q_t*α,Q_t*ρ,       s_t**σ,Q_t*Μ,Q_t Σ,Q_t Ι
+#define οαρσΜΣΙ         ω,    α,    ρ,            σ,    Μ,    Σ,    Ι
+#define οαρσΜΣ          ω,    α,    ρ,            σ,    Μ,    Σ
 #define L printf("%lu %02lu %03lu %s\n", Ι, α[Μ[Ι]], ρ[Ι], __FUNCTION__);
 #define N(a)      void a(OARS)
 #define A(Ι, ...) { void*s[]={__VA_ARGS__};                   \
@@ -17,20 +17,20 @@ typedef struct s_t { union {
 #define P(Ι, ...) { void*s[]={__VA_ARGS__};                   \
                     Q_t ξ=sizeof(s)/sizeof(*s);               \
                     while(ξ)                                  \
-                      ο[Ι][--ρ[Ι]].v = s[--ξ]; }
-#define T(Ι)      ο[Ι][ρ[Ι]++]
+                      ω[Ι][--ρ[Ι]].v = s[--ξ]; }
+#define T(Ι)      ω[Ι][ρ[Ι]++]
 #include<stdio.h>
 #include<assert.h>
 N(go2       ) { ; Q_t ψ = T(Ι).Q, nc = ψ >> 6 & 7, ac = ψ >> 3 & 7, oc = ψ & 7;
                   for (Q_t i = 0; i < ac; i++) A(Ι, T(Ι).v);
                   ρ[Ι] += ac + oc;
                   nc ? R(Ι).c(οαρσΜΣΙ) : go2(οαρσΜΣΙ); }
-N(go1       ) { ; Q_t ψ = ο[Ι][ρ[Ι]++].Q, nc = ψ >> 6 & 7, ac = ψ >> 3 & 7, oc = ψ & 7;
+N(go1       ) { ; Q_t ψ = T(Ι).Q, nc = ψ >> 6 & 7, ac = ψ >> 3 & 7, oc = ψ & 7;
                   ρ[Ι] += nc;
                   for (Q_t i = 0; i < ac; i++) A(Ι, T(Ι).v);
                   ρ[Ι] += oc;
                   ac ? R(Ι).c(οαρσΜΣΙ) : go1(οαρσΜΣΙ); }
-N(go0       ) { ; Q_t ψ = ο[Ι][ρ[Ι]++].Q, nc = ψ >> 6 & 7, ac = ψ >> 3 & 7, oc = ψ & 7;
+N(go0       ) { ; Q_t ψ = T(Ι).Q, nc = ψ >> 6 & 7, ac = ψ >> 3 & 7, oc = ψ & 7;
                   ρ[Ι] += nc + ac;
                   for (Q_t i = 0; i < ac; i++) A(Ι, T(Ι).v);
                   oc ? R(Ι).c(οαρσΜΣΙ) : go0(οαρσΜΣΙ); }
@@ -57,12 +57,13 @@ N(_shen_    ) {L; Q_t pi3 = R(Ι).Q;
 N(aushena   ) {L; P(Ι, 040, 2, 2, 1, _shen_)  R(Ι).c(οαρσΜΣΙ); }
 N(da        ) {L; for(Q_t i=0;i<8;i++) {
                     P(i, 0111, end, next1, end);
-                    ο[i][1].v = ρ[i]; }
+                    ω[i][1].v = ρ[i]; }
                   R(Ι).c(οαρσΜΣΙ); }
 N(an        ) {L; for(Q_t i=0;i<8;i++) {
                     P(i, 0111, end, end, next1);
-                    ο[i][0].v = ρ[i]; }
+                    ω[i][0].v = ρ[i]; }
                   R(Ι).c(οαρσΜΣΙ); }
+N(ο         ) {L; R(Ι).c(οαρσΜΣΙ);}
 N(deda_     ) {L; A(Ι, "დედა", C)           go1(οαρσΜΣΙ); }
 N(dedam     ) {L; P(2, 010, deda_)          R(Ι).c(οαρσΜΣΙ); }
 N(ma_       ) {L; A(Ι, "მათ", C)            go1(οαρσΜΣΙ); }
@@ -73,15 +74,16 @@ N(_ckho_    ) {L;                           _shen_(οαρσΜΣΙ); }
 N(gamouckho ) {L; P(Ι, 040, 2,2,2, _ckho_)  R(Ι).c(οαρσΜΣΙ); }
 
 
-N(ntext     ) {L; A(Ι,  mamam, sakhli, shvils, aushena,
-                  da, dedam, mat, namckhvari, gamouckho)
+N(ntext     ) {L; A(Ι, mamam, sakhli, shvils,     aushena,
+                  da,  dedam, mat,    namckhvari, gamouckho)
                   R(Ι).c(οαρσΜΣΙ); }
+
 N(show      ) {L; ntext(οαρσΜΣΙ); }
 
 int main() {
   Q_t Σ;
   s_t b[12][Σ = 512];
-  s_t*ο[8]    = {b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7]};
+  s_t*ω[8]    = {b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7]};
   Q_t α[4]    = {0, 0, 0, 0};
   Q_t ρ[8]    = {Σ, Σ, Σ, Σ, Σ, Σ, Σ, Σ};
   s_t*σ[4]    = {b[8], b[9], b[10], b[11]};
@@ -89,9 +91,9 @@ int main() {
   Q_t Ι       = 0;
   for (Q_t i  = 0; i < 8; i++) {
     P(i, 0111, end, (i ? next1 : end), end);
-    ο[i][0].v = ρ[i];
-    ο[i][1].v = ρ[i];
-    ο[i][2].v = ρ[i];
+    ω[i][0].v = ρ[i];
+    ω[i][1].v = ρ[i];
+    ω[i][2].v = ρ[i];
   }
   A(0, B);
   show(οαρσΜΣΙ);
