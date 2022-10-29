98199bf4c36b7a2bfc2ebd45fa99dedec8490de6
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-10-30 00:58:17 +0400

    use O

--- src/main48-12.c
+++ src/main48-12.c
@@ -22,7 +22,7 @@ typedef struct s_t { union {
                             ρ[Ι] += Be;                                   \
                             for (Q_t i = 0; i < c##On; i++) A(Ι, T(Ι).v); \
                             ρ[Ι] += Af;                                   \
-                            c##On ? R(Ι).c(οαρσΜΣΙ) : goto##On(οαρσΜΣΙ); }
+                            c##On ? O : goto##On(οαρσΜΣΙ); }
 #define O                 R(Ι).c(οαρσΜΣΙ)
 #define T(Ι)              ω[Ι][ρ[Ι]++]
 #define P(Ι, ...)         { void*s[]={__VA_ARGS__};                       \
@@ -42,11 +42,11 @@ N(end       ) { ;  }
 N(next1     ) { ; goto1(οαρσΜΣ,(Ι+7)%8); }
 
 N(mama_     ) {L; A(Ι, "მამა", C)   goto1(οαρσΜΣΙ); }
-N(mamam     ) {L; P(2, 010, mama_)  R(Ι).c(οαρσΜΣΙ); }
+N(mamam     ) {L; P(2, 010, mama_)  O; }
 N(shvil_    ) {L; A(Ι, "შვილ", C)   goto1(οαρσΜΣΙ); }
-N(shvils    ) {L; P(3, 010, shvil_) R(Ι).c(οαρσΜΣΙ); }
+N(shvils    ) {L; P(3, 010, shvil_) O; }
 N(sakhl_    ) {L; A(Ι, "სახლ", C)   goto1(οαρσΜΣΙ); }
-N(sakhli    ) {L; P(1, 010, sakhl_) R(Ι).c(οαρσΜΣΙ); }
+N(sakhli    ) {L; P(1, 010, sakhl_) O; }
 N(_shen_    ) {L; const char* Oi = (assert(R(3).v==C), R(3).Q);
                   const char* S  = (assert(R(3).v==C), R(3).Q);
                   const char* Op = (assert(R(3).v==C), R(3).Q);
@@ -57,25 +57,25 @@ N(aushena   ) {L; P(Ι, 010, _shen_)
 N(da        ) {L; for(Q_t i=0;i<8;i++) {
                     P(i, 0111, end, next1, end);
                   }
-                  R(Ι).c(οαρσΜΣΙ); }
+                  O; }
 N(an        ) {L; for(Q_t i=0;i<8;i++) {
                     P(i, 0111, end, end, next1);
                     ω[i][0].v = ρ[i]; }
-                  R(Ι).c(οαρσΜΣΙ); }
-N(ο         ) {L; R(Ι).c(οαρσΜΣΙ);}
+                  O; }
+N(ο         ) {L; O;}
 N(deda_     ) {L; A(Ι, "დედა", C)       goto1(οαρσΜΣΙ); }
-N(dedam     ) {L; P(2, 010, deda_)      R(Ι).c(οαρσΜΣΙ); }
+N(dedam     ) {L; P(2, 010, deda_)      O; }
 N(ma_       ) {L; A(Ι, "მათ", C)        goto1(οαρσΜΣΙ); }
-N(mat       ) {L; P(3, 010, ma_)        R(Ι).c(οαρσΜΣΙ); }
+N(mat       ) {L; P(3, 010, ma_)        O; }
 N(namckhvar_) {L; A(Ι, "ნამცხვარ", C)   goto1(οαρσΜΣΙ); }
-N(namckhvari) {L; P(2, 010, namckhvar_) R(Ι).c(οαρσΜΣΙ); }
+N(namckhvari) {L; P(2, 010, namckhvar_) O; }
 N(_ckho_    ) {L;                       _shen_(οαρσΜΣΙ); }
 N(gamouckho ) {L; P(Ι, 010, _ckho_)
-                  R(Ι).c(οαρσ 03330 ΣΙ); }
+                  R(Ι).c(οαρσ 0113330 ΣΙ); }
 
 N(ntext     ) {L; A(Ι, mamam, sakhli, shvils,     aushena,
                    da, dedam, mat,    namckhvari, gamouckho
-                  ) R(Ι).c(οαρσΜΣΙ); }
+                  ) O; }
 
 N(show      ) {L; ntext(οαρσΜΣΙ); }
 
