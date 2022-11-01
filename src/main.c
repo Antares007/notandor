ff7a07358a1111e14f7d2d5d79997a8194e88aa6
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-11-01 16:01:26 +0400

    Advance on current ιndex

--- src/main.c
+++ src/main.c
@@ -4,9 +4,9 @@
 typedef unsigned long Q_t;
 typedef struct s_t  {union{ void*v; Q_t Q; void(*c)(OARS); };} s_t;
 #define N(a)        void a(OARS)
-#define A(Δ, ...)   { void*ζs[] = {__VA_ARGS__};                        \
+#define A(...)      { void*ζs[] = {__VA_ARGS__};                        \
                     for (Q_t ζi = 0;ζi < sizeof(ζs) / sizeof(*ζs);ζi++) \
-                      σ[Μ[(Δ+ι)%8]][α[Μ[(Δ+ι)%8]]++].Q  = ζs[ζi]; }
+                      σ[Μ[ι]][α[Μ[ι]]++].Q = ζs[ζi]; }
 #define R(Δ)        σ[Μ[(Δ+ι)%8]][--α[Μ[(Δ+ι)%8]]]
 #define G(O, B, A)  void goto##O(OARS) {                                \
                     Q_t ψ = ο[ι][ρ[ι]++].Q,                             \
@@ -35,10 +35,10 @@ G(1, c2,00+c0)
 G(0, c2+c1,00)
 #undef G
 #include <assert.h>
-N(B         ) {L; A(0, B); ι=(ι+7)%8, C(1); }
-N(end2      ) { ; A(0, 2); }
-N(end1      ) { ; A(0, 1); }
-N(end0      ) { ; A(0, 0); }
+N(B         ) {L; A(B); ι=(ι+7)%8, C(1); }
+N(end2      ) { ; A(2); }
+N(end1      ) { ; A(1); }
+N(end0      ) { ; A(0); }
 N(nxt2      ) { ; ι=(ι+7)%8, C(2); }
 N(nxt1      ) { ; ι=(ι+7)%8, C(1); }
 N(nxt0      ) { ; ι=(ι+7)%8, C(0); }
@@ -56,13 +56,13 @@ N(ან        ) {L; {
                   Q_t ψ = R(0).Q;
                   printf("%lu\n", ψ);L;
                   if(ψ == 0) ι=(ι+7)%8,C(1);
-                  else A(0, ψ); }
-N(Co        ) {L; A(0, Co)          C(1); }
-N(mama_     ) {L; A(0, "მამა", Co)  C(1); }
+                  else A(ψ); }
+N(Co        ) {L; A(Co)          C(1); }
+N(mama_     ) {L; A("მამა", Co)  C(1); }
 N(mamam     ) {L; P(2, 010, mama_)  O;    }
-N(shvil_    ) {L; A(0, "შვილ", Co)  C(1); }
+N(shvil_    ) {L; A("შვილ", Co)  C(1); }
 N(shvils    ) {L; P(3, 010, shvil_) O;    }
-N(sakhl_    ) {L; A(0, "სახლ", Co)  C(1); }
+N(sakhl_    ) {L; A("სახლ", Co)  C(1); }
 N(sakhli    ) {L; P(1, 010, sakhl_) O;    }
 N(_shen_    ) {L; const char* Oi = (assert(R(3).v==Co), R(3).Q);
                   const char* Ss = (assert(R(3).v==Co), R(3).Q);
@@ -74,11 +74,11 @@ N(aushena   ) {L; P(0, 010, _shen_);
                   Μ[(ι+2)%8] = 3;
                   Μ[(ι+3)%8] = 3;
                   O; }
-N(deda_     ) {L; A(0, "დედა", Co)      C(1); }
+N(deda_     ) {L; A("დედა", Co)      C(1); }
 N(dedam     ) {L; P(2, 010, deda_)      O; }            
-N(ma_       ) {L; A(0, "მათ", Co)       C(1); }
+N(ma_       ) {L; A("მათ", Co)       C(1); }
 N(mat       ) {L; P(3, 010, ma_)        O; }            
-N(namckhvar_) {L; A(0, "ნამცხვარ", Co)  C(1); }
+N(namckhvar_) {L; A("ნამცხვარ", Co)  C(1); }
 N(namckhvari) {L; P(1, 010, namckhvar_) O; }
 N(_ckho_    ) {L;                       _shen_(ο,α,ρ,σ,ι,Μ); }
 N(gamouckho ) {L; P(0, 010, _ckho_)
@@ -88,7 +88,7 @@ N(gamouckho ) {L; P(0, 010, _ckho_)
                   O; }
 N(xti_      ) {L; printf("xti_\n");     C(1);}
 N(xtis      ) {L; P(2, 010, xti_)       O; }
-N(nar       ) {L; A(0, mamam, shvils, sakhli, aushena,
+N(nar       ) {L; A(mamam, shvils, sakhli, aushena,
                    და, gamouckho, mat, namckhvari, dedam,
                    ან, xtis,
                   ) O; }
@@ -101,9 +101,9 @@ N(gaarchie  ) {L; Μ[(ι+1)%8] = 3; Μ[(ι+2)%8] = 3; Μ[(ι+3)%8] = 3; O; }
 N(S_);
 N(S_i       ) {L; P(1, 010, S_) O; }
 N(S_it      ) {L; P(5, 010, S_) O; }
-N(S_        ) {L; A(0, S_i, aris, "b", texti,
+N(S_        ) {L; A(S_i, aris, "b", texti,
                             anda,        S_i, da,   "a", texti) O; }
-N(nar_       ) {L; A(0, "baaa", texti, S_it, gaarchie) O; }
+N(nar_       ) {L; A("baaa", texti, S_it, gaarchie) O; }
  
 int main() {
   Q_t Σ;
@@ -116,5 +116,5 @@ int main() {
   Q_t Μ[]     = {0,0,0,0,0,0,0,0};
   for (Q_t i = 0; i < 8; i++)
     P(i, 0111, i?nxt2:end2, i?nxt1:end1, i?nxt0:end0);
-  A(0, B, nar) O;
+  A(B, nar) O;
 }
