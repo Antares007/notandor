55edb27798a84c94fcd5ae11e2cd59880d7d1234
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-11-05 05:48:40 +0400

    A&R

--- src/s4.c
+++ src/s4.c
@@ -2,28 +2,39 @@
 #define oars             ο,             α,             ρ,             σ
 typedef unsigned long Q_t;
 typedef struct s_t { union { Q_t Q; void *v; void (*c)(OARS); }; } s_t;
+
+#define A(α, ...) { const void *va_args[] = {__VA_ARGS__};                    \
+                      Q_t size = sizeof(va_args)/sizeof(*va_args);            \
+                      for (Q_t i=0; i<size; i++) (*α++).v=(void*)va_args[i]; }
+#define R(ρ, ...) { const void *va_args[] = {__VA_ARGS__};                    \
+                      Q_t size = sizeof(va_args)/sizeof(*va_args);            \
+                      while (size) (*--ρ).v=(void*)va_args[--size]; }
 #define N(argo) void argo(OARS)
-#define A(...) {  const void *va_args[] = {__VA_ARGS__};                       \
-                  Q_t size = sizeof(va_args) / sizeof(*va_args);               \
-                  for (Q_t i=0; i<size; i++) (*α++).v=(void*)va_args[i]; }
-#define o (α--,(*α).c(oars))
+
+#define o(α) (α--,(*α).c(oars))
 #include<assert.h>
 N(cont) {                                                            
   Q_t ray_no = (*--α).Q; assert(ray_no < 8);
   while(1) {
     Q_t ψ = (*ρ++).Q; assert(ψ <= 07777777);
-    Q_t ray_no_count = ψ >> ray_no * 3 & 7;
-    for(Q_t i = ray_no + 1; i < 8                  ; i++) ρ += ψ >> 3 * i & 7;
-    for(Q_t i = 0         ; i < ray_no_count       ; i++) (*α++).v = (*ρ++).v;
-    for(Q_t i = 0         ; i < ray_no             ; i++) ρ += ψ >> 3 * i & 7;
+    Q_t ray_no_count = ψ >> 3 * ray_no & 7;
+    for(Q_t i = ray_no + 1; i < 8           ; i++) ρ += ψ >> 3 * i & 7;
+    for(Q_t i = 0         ; i < ray_no_count; i++) (*α++).v = (*ρ++).v;
+    for(Q_t i = 0         ; i < ray_no      ; i++) ρ += ψ >> 3 * i & 7;
     if (ray_no_count) return (--α, (*α).c(oars));
   }
 }
 #include<stdio.h>
-N(B) { A(B, 1, cont) o; }
-N(add_) { Q_t r = (--α, (*--α).Q); Q_t l = (--α, (*--α).Q); A(l + r, B) o; }
-N(add ) { (*--ρ).v = add_, (*--ρ).Q = 010, o; }
-N(one ) { (*--ρ).v = B,    (*--ρ).Q = 1, (*--ρ).Q = 020, o; }
+N(B     ) { A(α, B, 1, cont) o(α); }
+N(add_  ) { Q_t r = (--α, (*--α).Q);
+            Q_t l = (--α, (*--α).Q);
+            A(α, l + r, B) o(α); }
+N(add   ) { R(ρ, 010, add_) o(α); }
+N(one   ) { R(ρ, 020, 1, B) o(α); }
+N(seven ) { A(α, one, one, add, one, add, one, add, one, add, one, add, one, add) o(α); }
+
+N(ping  ) { A(α,  ping) o(α); }
+N(show  ) { A(α, seven) o(α); }
 
 N(e4) { printf("e4 %p\n", ρ);}
 N(e3) { printf("e3 %lu, %p\n", (*--α).Q, ρ);}
@@ -39,13 +50,9 @@ int main() {
         α→ */    
     s_t b[256 + 256], *σ = b, *ο = σ + 256, *α = ο, *ρ = ο + 256;
     printf("%p\n", ρ);
-    (*--ρ).v=e0;(*--ρ).v=e1;(*--ρ).v=e2;(*--ρ).v=e3;(*--ρ).Q=9;(*--ρ).v=e4;(*--ρ).Q=012111;
-    (*--ρ).v=b0;(*--ρ).v=b1;(*--ρ).v=b2;(*--ρ).Q=0111;
-    A(B,  one,
-          one, add,
-          one, add,
-          one, add,
-          one, add,
-          one, add,
-          one, add) o;
+    R(ρ, 012111, e4,  9, e3, e2, e1, e0) 
+    R(ρ,   0111, b2, b1, b0)
+    R(ο, 012111, e4,  9, e3, e2, e1, e0) 
+    R(ο,   0111, b2, b1, b0)
+    A(α, B, show) o(α);
 }
