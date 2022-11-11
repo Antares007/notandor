4155302a38812cc6fc9038bb4a1abf8b161a4e21
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-11-11 19:19:08 +0400

    sync

--- src/e.c
+++ src/e.c
@@ -18,7 +18,7 @@ typedef struct s_t {
 #define Ψ(...) ΨX(ο, co, __VA_ARGS__), Ψ_
 #define S(...) ΨX(ρ, ca, __VA_ARGS__), ι = ρ[-1].Q
 #define O ρ[ι - 1].c(ο, α, ν, ρ, ι - 1, σ, τ)
-#define D LOG, O 
+#define D O //LOG, 
 #define C(x)                                                                   \
   ρ = x,                                                                       \
   ο = ο[-2].v,                                                                 \
@@ -36,18 +36,41 @@ void one  (OARS) { Ψ()(one_)(), D; }
 void add_ (OARS) { Q_t r=σ[--τ].Q; Q_t l=σ[--τ].Q; σ[τ++].Q = l+r,C(α); }
 void add  (OARS) { Ψ()(add_)(), D; }
 void e    (OARS) { Ψ()()(), D; }
-void t_   (OARS) { (void)ρ[--ι].v, C(α); }
+void t_   (OARS) {  char *m = ρ[--ι].v;
+                    Q_t pos = σ[--τ].Q;
+                    Q_t len = σ[--τ].Q;
+                    char *inp = σ[--τ].v;
+                    if (pos < len && inp[pos] == m[0])
+                      σ[τ++].v = inp, σ[τ++].Q = len, σ[τ++].Q = pos + 1, C(α);
+                    else 
+                      σ[τ++].v = inp, σ[τ++].Q = len, σ[τ++].Q = pos, C(ο);
+                 }
 void t    (OARS) { Ψ()(ρ[--ι].v, t_)(), D; }
-void eOs_ (OARS) { S(e), D;
-                   S("s", t), D; }
-void eOs  (OARS) { Ψ()(eOs_)(), D;}
+void eOs  (OARS);
+void eOs_ (OARS) {  s_t nσ[128]; for(Q_t i=0;i<τ;i++) nσ[i].v = σ[i].v;
+                    S(e), D;
+                    σ = nσ;
+                    S("s", t, eOs, eOs), D; }
+void eOs  (OARS) {  Ψ()(eOs_)(), D;}
+void Sa   (OARS);
+void Sa_  (OARS) {  
+                    s_t nσ[128]; for(Q_t i=0;i<τ;i++) nσ[i].v = σ[i].v;
+                    s_t bσ[128]; for(Q_t i=0;i<τ;i++) bσ[i].v = σ[i].v;
+                    S("b", t), D; σ = nσ;
+                    S(Sa, "a", t), D; σ = bσ;
+                    S(Sa, "c", t), D; 
+                 }
+void Sa   (OARS) {  Ψ()(Sa_)(), D; }
 int main() {
   Q_t ι;
   s_t *ο, *α, *ν, *ρ = ο = α = ν = ι = 0;
   s_t σ[128];
   Q_t τ = 0;
   Ψ(e0)(e1)(e2);
-  S(eOs), D;
-  S(one, one, add, one, add), D;
-  S(one, one, add, one, add, one, add, one, add, one, add, one, add), D;
+  σ[τ++].v = "baaaaaaaa";
+  σ[τ++].Q = 9;
+  σ[τ++].Q = 0;
+  S(Sa), D;
+  //S(one, one, add, one, add), D;
+  //S(one, one, add, one, add, one, add, one, add, one, add, one, add), D;
 }
