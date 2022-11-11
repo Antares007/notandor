da746853104ac1fa0bae0a109a86d6c60b871877
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-11-11 13:43:18 +0400

    simplify dont chain sigma

--- src/e.c
+++ src/e.c
@@ -11,36 +11,34 @@ typedef struct s_t {
 #include <stdio.h>
 #define LOG printf("%s\n", __FUNCTION__)
 #define NUMARGS(...) (sizeof((int[]){__VA_ARGS__}) / sizeof(int))
-#define ΨX(X, ...)                                                             \
-  X = (s_t *)(const void *[]){0, 0, __VA_ARGS__, ο, α, ρ},                     \
-  X[0].Q = NUMARGS(__VA_ARGS__) + 5
+#define ΨX(X, ...) X = (s_t *)(const void *[]){X, 0, __VA_ARGS__},  \
+                  (X += 2)[-1].Q = NUMARGS(__VA_ARGS__)
 #define Ψ__(...) ΨX(ρ, __VA_ARGS__)
 #define Ψ_(...) ΨX(α, __VA_ARGS__), Ψ__
 #define Ψ(...) ΨX(ο, __VA_ARGS__), Ψ_
-#define S(...) ΨX(σ, __VA_ARGS__), ι = NUMARGS(__VA_ARGS__) + 2
+#define S(...) ΨX(σ, __VA_ARGS__), ι = σ[-1].Q
 #define O σ[ι - 1].c(ο, α, ρ, σ, ι - 1)
-#define D LOG, O
+#define D O //LOG, 
 #define C(α)                                                                   \
-  α[1].v = σ, σ = α, ο = σ[σ[0].Q - 3].v, α = σ[σ[0].Q - 2].v,                 \
-  ρ = σ[σ[0].Q - 1].v, ι = σ[0].Q - 3, D
-void nar(OARS) { C(α); }
-void e2(OARS) { LOG; }
-void e1(OARS) { printf("%lu\n", σ[1].s[2].Q); }
-void e0(OARS) { LOG; }
-void ob(OARS) { C(α); }
-void os(OARS) {
-  σ = σ[ι - 1].v;
-  D;
-}
-void one(OARS) { Ψ(os)(1, ob)(os), D; }
-void add_(OARS) {
-  Ψ(os)(σ[1].s[2].Q + σ[1].s[1].s[2].Q, ob)(os);
-  C(α);
-}
-void add(OARS) { Ψ(os)(add_)(os), D; }
+  σ = α,                                                                       \
+  ο = ο[-2].v,                                                             \
+  α = α[-2].v,                                                             \
+  ρ = ρ[-2].v,                                                             \
+  ι = σ[-1].Q, D
+void nar  (OARS) { C(α); }
+void e2   (OARS) { LOG; }
+void e1   (OARS) { LOG; printf("\n"); }
+void e0   (OARS) { LOG; }
+void ob   (OARS) { C(α); }
+void os   (OARS) { σ = σ[ι - 1].v, D; }
+void one_ (OARS) { C(α); }
+void one  (OARS) { Ψ(os)(one_)(os), D; }
+void add_ (OARS) { C(α); }
+void add  (OARS) { Ψ(os)(add_)(os), D; }
 int main() {
   Q_t ι;
   s_t *ο, *α, *ρ, *σ = ο = α = ρ = ι = 0;
   Ψ(e0)(e1)(e2);
-  S(ob, one, one, add, one, add, one, add, one, add, one, add, one, add), D;
+  S(ob, one, one, add), D;
+  S(ob, one, one, add), D;
 }
