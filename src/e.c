5b28c745de231e783754825b0faf2034ea0d26af
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-11-11 12:16:02 +0400

    use macros

--- src/e.c
+++ src/e.c
@@ -22,6 +22,12 @@ typedef struct s_t {
 //                      one, add) D; }
 #include <stdio.h>
 #define LOG printf("%s\n", __FUNCTION__)
+#define NUMARGS(...)  (sizeof((int[]){__VA_ARGS__})/sizeof(int))
+#define ΨX(X, ...) X = (s_t*)(const void*[]){0,0,__VA_ARGS__,ο,α,ρ}, X[0].Q=NUMARGS(__VA_ARGS__)+5
+#define Ψ__(...) ΨX(ρ, __VA_ARGS__)
+#define Ψ_(...)  ΨX(α, __VA_ARGS__),Ψ__
+#define Ψ(...)   ΨX(ο, __VA_ARGS__),Ψ_
+#define S(...) ΨX(σ, __VA_ARGS__), ι = NUMARGS(__VA_ARGS__) + 2
 #define O σ[ι - 1].c(ο, α, ρ, σ, ι - 1)
 #define D LOG, O
 #define C(α)                                                                   \
@@ -30,47 +36,23 @@ typedef struct s_t {
     ο          = o##α[o##α[0].Q - 3].v;                                        \
     α          = o##α[o##α[0].Q - 2].v;                                        \
     ρ          = o##α[o##α[0].Q - 1].v;                                        \
-    o##α[0].Q -= 3;                                                            \
     o##α[1].v  = σ;                                                            \
     σ          = o##α;                                                         \
-    ι          = σ[0].Q;                                                       \
+    ι          = σ[0].Q-3;                                                     \
   }                                                                            \
   D
-void nar(OARS) { C(α); }
-void e2(OARS) { LOG; }
-void e1(OARS) { printf("%lu\n", σ[1].s[2].Q); }
-void e0(OARS) { LOG; }
-void ob(OARS) { C(α); }
-void os(OARS) { σ = σ[ι-1].v; D; }
-void one(OARS) {
-  ο = (s_t[]){{{6}}, {{σ}}, {{os}},         {{ο}},{{α}},{{ρ}}};
-  α = (s_t[]){{{7}}, {{σ}}, {{1 }}, {{ob}}, {{ο}},{{α}},{{ρ}}};
-  ρ = (s_t[]){{{6}}, {{σ}}, {{os}},         {{ο}},{{α}},{{ρ}}};
-  D;
-}
-void add_(OARS) { 
-  ο = (s_t[]){{{6}}, {{σ}}, {{os}},         {{ο}},{{α}},{{ρ}}};
-  α = (s_t[]){{{7}}, {{σ}}, {{σ[1].s[2].Q + σ[1].s[1].s[2].Q }}, {{ob}}, {{ο}},{{α}},{{ρ}}};
-  ρ = (s_t[]){{{6}}, {{σ}}, {{os}},         {{ο}},{{α}},{{ρ}}};
-  C(α);
-}
-void add(OARS) {
-  ο = (s_t[]){{{6}}, {{σ}}, {{os  }}, {{ο}},{{α}},{{ρ}}};
-  α = (s_t[]){{{6}}, {{σ}}, {{add_}}, {{ο}},{{α}},{{ρ}}};
-  ρ = (s_t[]){{{6}}, {{σ}}, {{os  }}, {{ο}},{{α}},{{ρ}}};
-  D;
-}
-int main() {
-  s_t ο[] = {{{6}}, {{0}}, {{e0}}, {{0}}, {{0}}, {{0}}};
-  s_t α[] = {{{6}}, {{0}}, {{e1}}, {{0}}, {{0}}, {{0}}};
-  s_t ρ[] = {{{6}}, {{0}}, {{e2}}, {{0}}, {{0}}, {{0}}};
-  s_t σ[] = {{{16}}, {{0}}, {{ob}}, {{one}}, {{one}}, {{add}},
-                                             {{one}}, {{add}},
-                                             {{one}}, {{add}},
-                                             {{one}}, {{add}},
-                                             {{one}}, {{add}},
-                                             {{one}}, {{add}},
-                                            };
-  Q_t ι   = σ[0].Q;
-  D;
+void nar  (OARS) { C(α); }
+void e2   (OARS) { LOG; }
+void e1   (OARS) { printf("%lu\n", σ[1].s[2].Q); }
+void e0   (OARS) { LOG; }
+void ob   (OARS) { C(α); }
+void os   (OARS) { σ = σ[ι-1].v; D; }
+void one  (OARS) { Ψ(os)(1, ob)(os), D; }
+void add_ (OARS) { Ψ(os)(σ[1].s[2].Q + σ[1].s[1].s[2].Q, ob)(os); C(α); }
+void add  (OARS) { Ψ(os)(add_)(os), D; }
+int main  () {
+  Q_t ι;
+  s_t *ο, *α, *ρ, *σ = ο = α = ρ = ι = 0;
+  Ψ(e0)(e1)(e2);
+  S(ob, one, one, add, one, add, one, add, one, add, one, add, one, add), D;
 }
