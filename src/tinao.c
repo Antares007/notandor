99da8808fb974f7c4335aec722d557f8d8f6db68
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-11-16 15:06:40 +0400

    can lrec parse tina

--- src/tinao.c
+++ src/tinao.c
@@ -14,8 +14,9 @@ void term_(void **t, long i, void **n, long a, void **o) {
   const char *in = n[1];
   long len = n[2];
   long pos = n[3];
-  if (pos < len && strcmp(match_str, &in[pos]) <= 0)
-    c(t, i, T(n, in, len, pos + strlen(match_str)), a, o);
+  if (pos < len && strcmp(match_str, &in[pos]) <= 0 &&
+      (in[pos] == match_str[0]))
+    assert(a == 0), c(t, i, T(n, in, len, pos + strlen(match_str)), a, o);
   else
     assert(a == 0), c(0, 0, n, 0, o);
 }
@@ -23,21 +24,27 @@ void term(void **t, long i, void **n, long a, void **o) {
   const char *match_str = t[--a];
   c(t, i, n, a, T(0, T(match_str, term_), 0, o));
 }
+void ain_(void **t, long i, void **n, long a, void **o) {
+  c(t, i, n, a, T(0, T("a", term), 0, o));
+  c(t, i, n, a, T(0, T("i", term), 0, o));
+  c(t, i, n, a, T(0, T("n", term), 0, o));
+}
+void ain(void **t, long i, void **n, long a, void **o) {
+  c(t, i, n, a, T(0, T(ain_), 0, o));
+}
 void tina(void **t, long i, void **n, long a, void **o);
 void tina_(void **t, long i, void **n, long a, void **o) {
-  c(t, i, n, a, T(0, T("tina", term), 0, o));
-  c(t, i, n, a, T(0, T(tina, "a", term), 0, o));
-  c(t, i, n, a, T(0, T(tina, "b", term), 0, o));
-  c(t, i, n, a, T(0, T(tina, "c", term), 0, o));
+  c(t, i, n, a, T(0, T("t", term), 0, o));
+  c(t, i, n, a, T(0, T(tina, ain), 0, o));
 }
 void tina(void **t, long i, void **n, long a, void **o) {
   c(t, i, n, a, T(0, T(tina_), 0, o));
 }
 void e2(void **t, long i, void **n, long a, void **o) { LOG; }
 void e1(void **t, long i, void **n, long a, void **o) { LOG; printf("%p\n", n[3]); }
-void e0(void **t, long i, void **n, long a, void **o) { LOG; }
+void e0(void **t, long i, void **n, long a, void **o) { }
 int main() {
-  c(T(tina), 1, T(0, "tinacba", 7, 0), 1,
+  c(T(tina), 1, T(0, "tina", 4, 0), 1,
     T(T(e0),
       T(e1),
       T(e2),
