e1457a81faa04427bed6080eb24d735c89ba26fe
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-11-16 10:10:55 +0400

    ctinao

--- src/c.c
+++ src/c.c
@@ -1,22 +1,23 @@
 #define Λ(array) ((long)((void **)(array))[-1]) // count elements of an array
-void c(void **t, long i, void **n, long a, void **o) {
-  if (a)
+void c(void(c)(), void **t, long i, void **n, long a, void **o) {
+  if (a) {
     if (t[a] == c) {
       long ii = t[a - 1];
       if (o[ii]) {
-        c(o[ii], ii, n, Λ(o[ii]), o[Λ(o) - 1]);
-        if (1 < a) c(t, i, n, a - 2, o);
+        c(c, o[ii], ii, n, Λ(o[ii]), o[Λ(o) - 1]);
+        if (1 < a)
+          c(c, t, i, n, a - 2, o);
       } else {
-        c(t, i, n, a, o[Λ(o) - 1]);
+        c(c, t, i, n, a, o[Λ(o) - 1]);
       }
     } else {
-      ((void (*)())t[a - 1])(t, i, n, a - 1, o);
+      ((void (*)())t[a - 1])(c, t, i, n, a - 1, o);
     }
-  else {
+  } else {
     if (o[i]) {
-      c(o[i], i, n, Λ(o[i]), o[Λ(o) - 1]);
+      c(c, o[i], i, n, Λ(o[i]), o[Λ(o) - 1]);
     } else {
-      c(t, i, n, a, o[Λ(o) - 1]);
+      c(c, t, i, n, a, o[Λ(o) - 1]);
     }
   }
 }
--- src/tinao.c
+++ src/tinao.c
@@ -2,44 +2,41 @@
 #define T(...)                                                                 \
   ((void *)&(const void *[]){NumOfArgs(__VA_ARGS__), __VA_ARGS__}[1])
 #define Λ(a) ((long)(a)[-1])
-
 #define LOG printf("%s\n", __FUNCTION__)
 #include <assert.h>
 #include <stdio.h>
-void c(void **t, long i, void **n, long a, void **o);
-
 #include <string.h>
-void term_(void **t, long i, void **n, long a, void **o) {
+void term_(void(c)(), void **t, long i, void **n, long a, void **o) {
   const char *match_str = t[--a];
   const char *in = n[1];
   long len = n[2];
   long pos = n[3];
   if (pos < len && strcmp(match_str, &in[pos]) <= 0)
-    c(t, i, T(n, in, len, pos + strlen(match_str)), a, o);
+    c(c, t, i, T(n, in, len, pos + strlen(match_str)), a, o);
   else
-    assert(a == 0), c(0, 0, n, 0, o);
+    assert(a == 0), c(c, 0, 0, n, 0, o);
 }
-void term(void **t, long i, void **n, long a, void **o) {
+void term(void(c)(), void **t, long i, void **n, long a, void **o) {
   const char *match_str = t[--a];
-  c(t, i, n, a, T(0, T(match_str, term_), 0, o));
+  c(c, t, i, n, a, T(0, T(match_str, term_), 0, o));
 }
-void tina(void **t, long i, void **n, long a, void **o);
-void tina_(void **t, long i, void **n, long a, void **o) {
-  c(t, i, n, a, T(0, T("tina", term), 0, o));
-  c(t, i, n, a, T(0, T(tina, "a", term), 0, o));
-  c(t, i, n, a, T(0, T(tina, "b", term), 0, o));
-  c(t, i, n, a, T(0, T(tina, "c", term), 0, o));
+void tina(void(c)(), void **t, long i, void **n, long a, void **o);
+void tina_(void(c)(), void **t, long i, void **n, long a, void **o) {
+  c(c, t, i, n, a, T(0, T("tina", term), 0, o));
+  c(c, t, i, n, a, T(0, T(tina, "a", term), 0, o));
+  c(c, t, i, n, a, T(0, T(tina, "b", term), 0, o));
+  c(c, t, i, n, a, T(0, T(tina, "c", term), 0, o));
 }
-void tina(void **t, long i, void **n, long a, void **o) {
-  c(t, i, n, a, T(0, T(tina_), 0, o));
+void tina(void(c)(), void **t, long i, void **n, long a, void **o) {
+  c(c, t, i, n, a, T(0, T(tina_), 0, o));
 }
-void e2(void **t, long i, void **n, long a, void **o) { LOG; }
-void e1(void **t, long i, void **n, long a, void **o) { LOG; printf("%p\n", n[3]); }
-void e0(void **t, long i, void **n, long a, void **o) { LOG; }
+void e2(void(c)(), void **t, long i, void **n, long a, void **o) { LOG; }
+void e1(void(c)(), void **t, long i, void **n, long a, void **o) {
+  LOG;
+  printf("%p\n", n[3]);
+}
+void e0(void(c)(), void **t, long i, void **n, long a, void **o) { LOG; }
+void c(void(c)(), void **t, long i, void **n, long a, void **o);
 int main() {
-  c(T(tina), 1, T(0, "tinacba", 7, 0), 1,
-    T(T(e0),
-      T(e1),
-      T(e2),
-      0));
+  c(c, T(tina), 1, T(0, "tinacba", 7, 0), 1, T(T(e0), T(e1), T(e2), 0));
 }
