4e1269c6989bf414a6950d4492f4294ce80f4e16
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-11-16 09:30:19 +0400

    can parse left recurcive grammar

--- src/tinao.c
+++ src/tinao.c
@@ -1,34 +1,45 @@
 #define NumOfArgs(...) (sizeof((const void *[]){__VA_ARGS__}) / sizeof(void *))
-#define T(...) ((void *)&(void *[]){NumOfArgs(__VA_ARGS__), __VA_ARGS__}[1])
+#define T(...)                                                                 \
+  ((void *)&(const void *[]){NumOfArgs(__VA_ARGS__), __VA_ARGS__}[1])
 #define Λ(a) ((long)(a)[-1])
 
 #define LOG printf("%s\n", __FUNCTION__)
 #include <assert.h>
 #include <stdio.h>
-void c(void **o, void **t, long i, void **n, long a);
-//void t(void **o, void **t, long i, void **n, long a) {}
-//void tina(void **o, void **t, long i, void **n, long a);
-//void tina_(void **o, void **t, long i, void **n, long a) {
-//  r(T(0, T("tina", t), 0, o), t, i, n, a);
-//  r(T(0, T(tina, "a", t), 0, o), t, i, n, a);
-//}
-//void tina(void **o, void **t, long i, void **n, long a) {
-//  run(T(0, T(tina_), 0, o), t, i, n, a);
-//}
-void e2(void **t, long i, void **n, long a, void **o) { LOG; n[2]--; }
-void e1(void **t, long i, void **n, long a, void **o) { LOG; n[1]--; }
-void e0(void **t, long i, void **n, long a, void **o) { LOG; n[0]--; }
+void c(void **t, long i, void **n, long a, void **o);
+
+#include <string.h>
+void term_(void **t, long i, void **n, long a, void **o) {
+  const char *match_str = t[--a];
+  const char *in = n[1];
+  long len = n[2];
+  long pos = n[3];
+  if (pos < len && strcmp(match_str, &in[pos]) <= 0)
+    c(t, i, T(n, in, len, pos + strlen(match_str)), a, o);
+  else
+    assert(a == 0), c(0, 0, n, 0, o);
+}
+void term(void **t, long i, void **n, long a, void **o) {
+  const char *match_str = t[--a];
+  c(t, i, n, a, T(0, T(match_str, term_), 0, o));
+}
+void tina(void **t, long i, void **n, long a, void **o);
+void tina_(void **t, long i, void **n, long a, void **o) {
+  c(t, i, n, a, T(0, T("tina", term), 0, o));
+  c(t, i, n, a, T(0, T(tina, "a", term), 0, o));
+  c(t, i, n, a, T(0, T(tina, "b", term), 0, o));
+  c(t, i, n, a, T(0, T(tina, "c", term), 0, o));
+}
+void tina(void **t, long i, void **n, long a, void **o) {
+  c(t, i, n, a, T(0, T(tina_), 0, o));
+}
+void e2(void **t, long i, void **n, long a, void **o) { LOG; }
+void e1(void **t, long i, void **n, long a, void **o) { LOG; printf("%p\n", n[3]); }
+void e0(void **t, long i, void **n, long a, void **o) { LOG; }
 int main() {
-  void**s;
-  c(0, 1, s=T(7,7,7), 0,
-    T(T(1, c),
-      T(2, c, 1, c, 0, c, 2, c, 1, c, 0, c),
-      T(0, c),
-      T(T(2,c),
-        T(0,c),
-        T(1,c),
-        T(T(e0),
-          T(e1),
-          T(e2),
-          0))));
+  c(T(tina), 1, T(0, "tinacba", 7, 0), 1,
+    T(T(e0),
+      T(e1),
+      T(e2),
+      0));
 }
