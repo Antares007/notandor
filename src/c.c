cf7b2bed7519276b7f46a14ec22aa68b1c2397e2
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-11-16 12:02:12 +0400

    format c.c

--- src/c.c
+++ src/c.c
@@ -1,23 +1,18 @@
 #define Λ(array) ((long)((void **)(array))[-1]) // count elements of an array
 void c(void(c)(), void **t, long i, void **n, long a, void **o) {
-  if (a) {
+  if (a)
     if (t[a] == c) {
       long ii = t[a - 1];
       if (o[ii]) {
         c(c, o[ii], ii, n, Λ(o[ii]), o[Λ(o) - 1]);
         if (1 < a)
           c(c, t, i, n, a - 2, o);
-      } else {
+      } else
         c(c, t, i, n, a, o[Λ(o) - 1]);
-      }
-    } else {
+    } else
       ((void (*)())t[a - 1])(c, t, i, n, a - 1, o);
-    }
-  } else {
-    if (o[i]) {
-      c(c, o[i], i, n, Λ(o[i]), o[Λ(o) - 1]);
-    } else {
-      c(c, t, i, n, a, o[Λ(o) - 1]);
-    }
-  }
+  else if (o[i])
+    c(c, o[i], i, n, Λ(o[i]), o[Λ(o) - 1]);
+  else
+    c(c, t, i, n, a, o[Λ(o) - 1]);
 }
