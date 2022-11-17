8a3e1282f264876777b4f126da0a2b2a0b2dacc7
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-11-17 08:34:41 +0400

    seven with new api

--- src/t.c
+++ src/t.c
@@ -6,25 +6,35 @@
 #include <stdio.h>
 #define LOG printf("%.2ld %s\n", r, __FUNCTION__);
 
-void c(void (***o)(), void (**a)(), long r, void **s) {
-  LOG;
+void c(void (***o)(), void (**a)(), long r, void **s) { LOG;
   o[r][(long)o[r][-1] - 1](o[-2], &o[r][(long)o[r][-1] - 1], r, s);
 }
 void one_(void (***o)(), void (**a)(), long r, void **s) {
-  LOG;
-  c(o, a, r, s);
+  c(o, a, r, O(s, 1));
 }
-void one(void (***o)(), void (**a)(), long r, void **s) {
-  LOG;
-  a[-1](O(o, T(c), T(c), T(c)), a - 1, r, s);
+void one(void (***o)(), void (**a)(), long r, void **s) { LOG;
+  a[-1](O(o, T(c), T(c, one_), T(c)), a - 1, r, s);
+}
+void add_(void (***o)(), void (**a)(), long r, void **s) { LOG;
+  void**sp = s[-2];
+  c(o, a, r, O(sp[-2], (long)sp[0] + (long)s[0]));
+}
+void add(void (***o)(), void (**a)(), long r, void **s) { LOG;
+  a[-1](O(o, T(c), T(add_), T(c)), a - 1, r, s);
 }
 
 void thankyouGod(void (***a)()) {}
 
-void e0(void **o, long n, long m, void **s) { printf("%s\n", __FUNCTION__); }
-void e1(void **o, long n, long m, void **s) { printf("%s\n", __FUNCTION__); }
-void e2(void **o, long n, long m, void **s) { printf("%s\n", __FUNCTION__); }
+void e0(void(***o)(), void(**a)(), long r, void **s) { printf("%s\n", __FUNCTION__); }
+void e1(void(***o)(), void(**a)(), long r, void **s) { printf("%p %s\n", s[0], __FUNCTION__); }
+void e2(void(***o)(), void(**a)(), long r, void **s) { printf("%s\n", __FUNCTION__); }
 int main() {
-  void (***o)() = O(0, T(e0), T(e1), T(e2));
-  c(o, 0, 2, 0);
+  void (***o)() = O(O(0,
+                      T(e0),
+                      T(e1),
+                      T(e2)),
+                        T(c),
+                        T(c,one,one,add,one,add,one,add,one,add,one,add,one,add),
+                        T(c));
+  c(o, 0, 1, 0);
 }
