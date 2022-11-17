f216d3fdd5e5a8e6da02b37536a8ea884d3c4981
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-11-17 09:15:40 +0400

    left rec with new api

--- src/t.c
+++ src/t.c
@@ -4,7 +4,7 @@
 #define O(o, ...) ((void *)&VA(o, Nargs(__VA_ARGS__), __VA_ARGS__)[2])
 #include <assert.h>
 #include <stdio.h>
-#define LOG printf("%.2ld %s\n", r, __FUNCTION__);
+#define LOG //printf("%.2ld %s\n", r, __FUNCTION__);
 
 void c(void (***o)(), void (**a)(), long r, void **s) { LOG;
   o[r][(long)o[r][-1] - 1](o[-2], &o[r][(long)o[r][-1] - 1], r, s);
@@ -13,7 +13,7 @@ void one_(void (***o)(), void (**a)(), long r, void **s) { LOG;
   c(o, a, r, O(s, 1));
 }
 void one(void (***o)(), void (**a)(), long r, void **s) { LOG;
-  a[-1](O(o, T(c), T(c, one_), T(c)), a - 1, r, s);
+  a[-1](O(o, T(c), T(one_), T(c)), a - 1, r, s);
 }
 void add_(void (***o)(), void (**a)(), long r, void **s) { LOG;
   void**sp = s[-2];
@@ -24,17 +24,60 @@ void add(void (***o)(), void (**a)(), long r, void **s) { LOG;
 }
 
 void thankyouGod(void (***a)()) {}
-
-void e0(void(***o)(), void(**a)(), long r, void **s) { printf("%s\n", __FUNCTION__); }
-void e1(void(***o)(), void(**a)(), long r, void **s) { printf("%p %s\n", s[0], __FUNCTION__); }
+void printo(void(***o)(), void(**a)(), long r, void **s) {
+  while(o) {
+    printf("%p %p %p\n", o[0][-1], o[1][-1], o[2][-1]);
+    o = (void*)o[-2];
+  }
+  a[-1](o, a - 1, r, s);
+}
+void e0(void(***o)(), void(**a)(), long r, void **s) { /*printf("%s\n", __FUNCTION__);*/ }
+void e1(void(***o)(), void(**a)(), long r, void **s) { printf("%s %ld %ld %s\n", (char*)s[0], (long)s[1], (long)s[2], __FUNCTION__); }
 void e2(void(***o)(), void(**a)(), long r, void **s) { printf("%s\n", __FUNCTION__); }
+
+void term_(void(***o)(), void(**a)(), long r, void **s) { LOG;
+  const char *m   = (void*)*--a;
+  const char *in  = s[0];
+  long len        = s[1];
+  long pos        = s[2];
+  if (pos < len && in[pos] == m[0])
+    c(o,a,r,O(s, in, len, pos + 1));
+  else
+    c(o,a,0,s);
+}
+void term(void(***o)(), void(**a)(), long r, void **s) { LOG;
+  const char *m = (void*)*--a;
+  (void)m;
+  a[-1](O(o,
+          T(c),
+          T(m, term_),
+          T(c)), a - 1, r, s);
+}
+void tina(void(***o)(), void(**a)(), long r, void **s);
+void tina_(void(***o)(), void(**a)(), long r, void **s) { LOG;
+  a[-1](O(o,
+          T(c),
+          T(c, "t", term),
+          T(c)), a - 1, r, s);
+  a[-1](O(o,
+          T(c),
+          T(c, tina, "a", term),
+          T(c)), a - 1, r, s);
+}
+void tina(void(***o)(), void(**a)(), long r, void **s) { LOG;
+  a[-1](O(o,
+          T(c),
+          T(c, tina_),
+          T(c)), a - 1, r, s);
+}
+
 int main() {
   void (***o)() = O(O(0,
                       T(e0),
                       T(e1),
                       T(e2)),
                         T(c),
-                        T(c,one,one,add,one,add,one,add,one,add,one,add,one,add),
+                        T(c, tina), //T(c,one,one,add,one,add,one,add,one,add,one,add,one,add),
                         T(c));
-  c(o, 0, 1, 0);
+  c(o, 0, 1, O(0, "taaa", 4, 0));
 }
