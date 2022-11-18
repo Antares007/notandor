eac936bbf999acda2fd745fd15a56cc7ccfcba32
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-11-18 08:15:57 +0400

    fix order

--- src/h.c
+++ src/h.c
@@ -21,9 +21,9 @@ N(m) {
   long i=0;
   while(ms[i]) {
     if (ms[i] < in[pos+i])
-      return r--, o[r][-1](o[-2], o[r]-1, r, s);
-    else if (in[pos+i] < ms[i])
       return r++, o[r][-1](o[-2], o[r]-1, r, s);
+    else if (in[pos+i] < ms[i])
+      return r--, o[r][-1](o[-2], o[r]-1, r, s);
     i++;
   }
   o[r][-1](o[-2], o[r]-1, r, O(s, in, pos + i));
@@ -33,14 +33,13 @@ N(ε) { LOG; }
 N(lt) { LOG; }
 N(gt) { LOG; }
 N(k) { LOG; printf("\"%s\" %ld\n", (char*)s[0], (long)s[1]), c(o, a, 4, s); }
-#define S(a) T(#a, m)
 // clang-format off
 #define S7(sw1,rw1,sw2,rw2,sw3,rw3,sw4,rw4,sw5,rw5,sw6,rw6,sw7,rw7) \
   O(O(O(O(O(0,                                                      \
-  T(gt)rw7 rw6 rw5 rw4 rw3 rw2 rw1 T(lt)),                          \
-  T(lp)sw7 T(c)T(c)T(c)T(c)T(c)sw1 T(ε )),                          \
-  T(c )T(ε)sw6 T(c)T(c)T(c)sw2 T(ε)T(ε )),                          \
-  T(c )T(ε)T(ε)sw5 T(c)sw3 T(ε)T(ε)T(ε )),                          \
+  T(lt)rw1 rw2 rw3 rw4 rw5 rw6 rw7 T(gt)),                          \
+  T(lp)sw1 T(c)T(c)T(c)T(c)T(c)sw7 T(ε )),                          \
+  T(c )T(ε)sw2 T(c)T(c)T(c)sw6 T(ε)T(ε )),                          \
+  T(c )T(ε)T(ε)sw3 T(c)sw5 T(ε)T(ε)T(ε )),                          \
   T(c )T(ε)T(ε)T(ε)sw4 T(ε)T(ε)T(ε)T(ε ))   
 int main() {
   void(***o)()  = S7(
