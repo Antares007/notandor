e8355edae51b54a63d4eb3b593df4c49b60d9157
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-11-22 05:10:27 +0400

    sync

--- src/s.c
+++ src/s.c
@@ -2,7 +2,7 @@
 #define VA(...) ((const void *[]){__VA_ARGS__})
 #define T(...)                                                                 \
   ((void *)&VA(__VA_ARGS__, Nargs(__VA_ARGS__))[Nargs(__VA_ARGS__)])
-#define O(o, ...) ((void *)&VA(o, Nargs(__VA_ARGS__), __VA_ARGS__)[2])
+#define B(o, ...) ((void *)&VA(o, Nargs(__VA_ARGS__), __VA_ARGS__)[2])
 #define N(argo, ...)                                                           \
   void argo(OARS) {                                                            \
     { __VA_ARGS__ }                                                            \
@@ -12,7 +12,7 @@
 #define D(o, s, op, os) LOG,a[-1](o, &a[-1], r, s, op, os), ident_count--
 #define Short(v) (long)(v) & (long)0xffff
 #define LOG                                                                    \
-  logn(o, a, r, s, op, os, __FUNCTION__, ident_count++), sleep(1)
+  logn(o, a, r, s, op, os, __FUNCTION__, ident_count++), usleep(100000)
 #include <assert.h>
 #include <stdio.h>
 #include <string.h>
@@ -31,31 +31,43 @@ N(cn,     long newray = *--a; C(newray, s, op, os);)
 
 N(term_,  const char *ms = os[0]; const char *in = op[0]; long len = op[1];
           long pos = s[0];
-          if (pos < len && in[pos] == ms[0]) C(r, O(s, pos + 1), op, os[-2]);
+          if (pos < len && in[pos] == ms[0]) C(r, B(s, pos + 1), op, os[-2]);
           else C(r-1, s, op, os[-2]);)
 
+N(Var,    void*sword = *--a; C(2, s, op, B(os, o, sword));)
+void S();
+N(cnlo,   
+          if (o && os[1] == o[1][2 - (long)o[1][0]]) printf("\nfound lrec\n");
+          C(r,s,op,os);)
 N(term,   const char *ms = (void *)*--a;
-          D(O(o, T(0, cn), T(1, cn, term_),         T(2, cn)), s, op, O(os, ms));) void S();
+          D(B(o,
+            T(0, cn), T(1, cn, term_),         T(2, cn)), s, op, B(os, ms));) void S();
 
-N(S_,     D(O(o, T(0, cn), T(1, cn,    "b", term),  T(2, cn)), s, op, os);
-          D(O(o, T(0, cn), T(1, cn, S, "a", term),  T(2, cn)), s, op, os);)
+N(S_,     D(B(o,
+            T(0, cn), T(1, cn,    "b", term),  T(2, cn)), s, op, os);
 
-N(S,      D(O(o, T(0, cn), T(1, cn, S_),            T(2, cn)), s, op, os);)
+          D(B(B(o,
+            T(0, cn), T(1, cn, S, "a", term),  T(2, cn)),
+            T(0, cn), T(1, cn),                T(cnlo)), s, op, os);)
 
+N(S,      D(B(o,
+            T(0, cn), T(S, Var, S_),           T(2, cn)), s, op, os);)
+
+N(nolrec, o = os[0]; C(1, s, op, os[-2]); )
 void init();
 int main() {
           init();
-          void (***o)() = O(0, T(r0), T(r1), T(r2));
+          void (***o)() = B(0, T(r0), T(r1), T(nolrec));
           void ( **a)() = T(cr, S);
           long r        = 1;
           void **s      = 0;
           void **op     = 0;
           void **os     = 0;
-          D(o, O(s, 0), O(op, "baaa", 4), os);
+          D(o, B(s, 0), B(op, "baaa", 4), os);
 }
 ////NNSD
 //#define NN(argo, ab, bb) N(argo##a, ab)N(argo##b, bb)
-// NN(s, { s = O(s, ((long)s[0]) + 1); C(1); }, { C(1); })
+// NN(s, { s = B(s, ((long)s[0]) + 1); C(1); }, { C(1); })
 char *names[0x10000] = {};
 ;
 #define NAME(me) names[(long)me & 0xffff] = #me
@@ -68,6 +80,9 @@ void init() {
   NAME(term);
   NAME(S_);
   NAME(S);
+  NAME(Var);
+  NAME(cnlo);
+  NAME(nolrec);
 }
 char*data = "NNSD";
 void logn(OARS, const char*name, long ident_count) {
