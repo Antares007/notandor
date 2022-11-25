ffddf9bfca196c21bacf1d806c3fe162ebc6d9f5
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-11-25 08:26:38 +0400

    better fix

--- src/s5.c
+++ src/s5.c
@@ -3,7 +3,6 @@
 #define T(...)                                                                 \
   ((void *)&V(cr, __VA_ARGS__, Nargs(__VA_ARGS__) + 1)[Nargs(__VA_ARGS__) + 1])
 #define B(o, ...) ((void *)&V(o, 0, Nargs(__VA_ARGS__), __VA_ARGS__)[3])
-typedef void (*fun_t)();
 #define D(o, s, op, os) LOG, a[-1](o, a - 1, r, s, op, os)
 #define oars                                                                   \
   void (***o)(), void (**a)(), long r, void (***s)(), void **op, void **os
@@ -11,37 +10,20 @@ typedef void (*fun_t)();
 #include <stdio.h>
 #include <unistd.h>
 void ps(oars) { puts((void *)*--a), D(o, s, op, os); }
-void cr(oars) { LOG, o[r][-1](o[-3], o[r] - 1, r, s, op, os); }
-void c0(oars) { LOG, o[0][-1](o[-3], o[0] - 1, 0, s, op, os); }
-void c1(oars) { LOG, o[1][-1](o[-3], o[1] - 1, 1, s, op, os); }
-void pop(oars) {
-  D(((void ***)op)[0][-3] = o, s, op, os);
-}
-void pos(oars) {
-  D(((void ***)os)[0][-3] = o, s, op, os);
-}
-void ob(oars) {
-  D(B(o, T(c0, pop), T(c0, pop), T(c0, pop), T(c0, pop)),
-    B(s, T(c0, pos), T(c0, pos), T(c0, pos), T(c0, pos)), op, os);
-}
-void soo0(oars) { D(s, o, op, os); }
-void soo1(oars) { D(s, o, op, os); }
-void soo2(oars) { D(s, o, op, os); }
-void soo3(oars) { D(s, o, op, os); }
-void sos0(oars) { D(s, o, op, os); }
-void sos1(oars) { D(s, o, op, os); }
-void sos2(oars) { D(s, o, op, os); }
-void sos3(oars) { D(s, o, op, os); }
+void cr(oars) { LOG, o[r][-1](o[-3], o[r] - 1, r, s, B(op, o), B(os, s)); }
+void c0(oars) { cr(o, a, 0, s, op, os); }
+void c1(oars) { cr(o, a, 1, s, op, os); }
+void so(oars) { D(os[0], op[0], os, op); }
 void o(oars) {
-  o = B(0, T(soo0), T(soo1), T(soo2), T(soo3));
-  D(o, s, B(op, o), os);
+  o = B(0, T("hi o", ps, so), T("send next", ps, so), T(so), T(so));
+  D(o, s, op, os);
 }
 void s(oars) {
-  s = B(0, T(c1, sos0), T(sos1), T(sos2), T(sos3));
-  D(o, s, op, B(os, s));
+  s = B(0, T(c1, "hello s", ps, so), T("take it", ps, so), T(so), T(so));
+  D(o, s, op, os);
 }
 int main() {
-  void (**a)() = T(ob, o, s);
+  void (**a)() = T(cr, o, s);
   long r = 0;
   D(0, 0, 0, 0);
 }
