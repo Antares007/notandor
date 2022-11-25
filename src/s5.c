dc70543e827db1c2ff26646151ec40ae476f7968
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-11-25 06:55:47 +0400

    loop

--- src/s5.c
+++ src/s5.c
@@ -7,14 +7,28 @@ typedef void (*fun_t)();
 #define D(o, s, op, os) LOG, a[-1](o, a - 1, r, s, op, os)
 #define oars                                                                   \
   void (***o)(), void (**a)(), long r, void (***s)(), void **op, void **os
-#define LOG printf("%ld %s\n",r , __FUNCTION__), usleep(220000)
+#define LOG printf("%ld %s\n", r, __FUNCTION__), usleep(220000)
 #include <stdio.h>
 #include <unistd.h>
-void cr(oars) { LOG, o[r][-1](o[-3], o[r]-1, r, s, op, os); }
-void stop(oars) { LOG; }
-void show(oars) { D(B(o, T(stop)), s, op, os); }
+void cr(oars) { LOG, o[r][-1](o[-3], o[r] - 1, r, s, op, os); }
+void c0(oars) { LOG, o[0][-1](o[-3], o[0] - 1, 0, s, op, os); }
+void c1(oars) { LOG, o[1][-1](o[-3], o[1] - 1, 1, s, op, os); }
+void pos(oars) { D(((void***)op)[0][-3] = o, ((void***)os)[0][-3] = s, op, os); }
+void ob(oars) {
+  D(B(o, T(c0, pos), T(c0, pos), T(c0, pos), T(c0, pos)),
+    B(s, T(c0, pos), T(c0, pos), T(c0, pos), T(c0, pos)), op, os);
+}
+void so(oars) { D(s, o, op, os); }
+void o(oars) {
+  o = B(0, T(so), T(so), T(so), T(so));
+  D(o, s, B(op, o), os);
+}
+void s(oars) {
+  s = B(0, T(c1, so), T(so), T(so), T(so));
+  D(o, s, op, B(os, s));
+}
 int main() {
-  void (**a)() = T(show);
+  void (**a)() = T(ob, o, s);
   long r = 0;
   D(0, 0, 0, 0);
 }
