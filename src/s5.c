5e307582fc5c6dcb2517e26ad0e20e9d522c7ca6
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-11-26 05:06:17 +0400

    clean

--- src/s5.c
+++ src/s5.c
@@ -3,35 +3,28 @@
 #define V(...)          ((const void *[]){__VA_ARGS__})
 #define T(...)          ((void *)&V(cr, __VA_ARGS__, Nargs(__VA_ARGS__) + 1)[Nargs(__VA_ARGS__) + 1])
 #define B(o, ...)       ((void *)&V(o, 0, Nargs(__VA_ARGS__), __VA_ARGS__)[3])
-#define D(o, s, op, os) LOG, a[-1](o, a - 1, r, s, op, os)
+#define D(o, s, op, os) a[-1](o, a - 1, r, s, op, os)
 #define oars            void (***o)(), void (**a)(), long r, void (***s)(), void **op, void **os
-#define LOG             printf("%ld %s\n", r, __FUNCTION__), usleep(125500)
+#define LOG             printf("%ld %s\n", r, __FUNCTION__), usleep(777777)
 #include <assert.h>
 #include <stdio.h>
 #include <unistd.h>
-void ps(oars) { puts((void *)*--a), D(o, s, op, os); }
-void cr(oars) { LOG, o[r][-1](o[-3], o[r] - 1, r, s, op, os); }
-void bo(oars) { LOG, o[r][-1](o[-3], o[r] - 1, r, s, B(op, o), B(os, s)); }
-void c0(oars) { cr(o, a, 0, s, op, os); }
-void c1(oars) { cr(o, a, 1, s, op, os); }
-void soos(oars) { printf("%ld\n", (long)o); D(os[0], op[0], os, op); }
-void osop(oars) { D(o, s, op, os); }
-void o0(oars) { D(os[0], op[0], os, op); }
-void o1(oars) { D(os[0], op[0], os, op); }
-void o2(oars) { D(os[0], op[0], os, op); }
-void o3(oars) { D(os[0], op[0], os, op); }
-void o(oars) { D(B(o, T(o0), T(o1), T(o2), T(o3)), s, op, os); }
-void s0(oars) { D(os[0], op[0], os, op); }
-void s1(oars) { D(os[0], op[0], os, op); }
-void s2(oars) { D(os[0], op[0], os, op); }
-void s3(oars) { D(os[0], op[0], os, op); }
-void s(oars) { D(o, B(s, T(c1, s0), T(s1), T(s2), T(s3)), op, os); }
-void m(oars) {
+void ps   (oars) { printf("%s\n", (char *)*--a), usleep(555555), D(o, s, op, os); }
+void cr   (oars) { o[r][-1](o[-3], o[r] - 1, r, s, op, os); }
+void bo   (oars) { LOG, o[r][-1](o[-3], o[r] - 1, r, s, B(op, o), B(os, s)); }
+void c0   (oars) { cr(o, a, 0, s, op, os); }
+void c1   (oars) { cr(o, a, 1, s, op, os); }
+void c2   (oars) { cr(o, a, 2, s, op, os); }
+void soos (oars) { D(os[0], op[0], os, op); }
+void osop (oars) { D(o, s, op, os); }
+void o    (oars) { D(B(o, T("o0", ps, soos), T("o1", ps, soos), T("o2", ps, soos), T("o3", ps, soos)), s, op, os); }
+void s    (oars) { D(o, B(s, T(c1, "s0", ps, soos), T("s1", ps, soos), T("s2", ps, soos), T("s3", ps, soos)), op, os); }
+void m    (oars) {
   D(B(o, T("map p0", ps, osop), T("map p1", ps, osop), T("map p2", ps, osop), T("map p3", ps, osop)),
     B(s, T("map c0", ps, osop), T("map c1", ps, osop), T("map c2", ps, osop), T("map c3", ps, osop)), op, os);
 }
 int main() {
-  void (**a)() = T(bo, m, o, s);
+  void (**a)() = T(bo, o, s, s);
   long r = 0;
   D(1, 0, 0, 0);
 }
