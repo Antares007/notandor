1d8b3747138d3016368f9f0ca165f710e479ea4a
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-11-29 21:13:40 +0400

    can parse with hummer

--- src/word.c
+++ src/word.c
@@ -22,55 +22,75 @@ void continues(obrt, rest) {
   t = (void *)*t[-1];
   D(o, t, frwd);
 }
-void terminal_ray(obrt, char *s) {
-  char*m = (char*)*--t;
-  if(m[0] = s[0]) D(o, t, s+1);
-  else D(o, t, s);
+void propeller(obrt, rest) {
+  long a = r;
+  for (r = 1; r < a; r++)
+    D(o, t, frwd);
+  for (r = a; r < 8; r++)
+    D(o, t, frwd);
 }
+void terminal_ray(obrt, char *s) {
+  char *m = (char *)*--t;
+  if (m[0] == s[0])
+    D(o, t, s + 1);
+}
+static void *Tpr = T(propeller);
 void terminal(obrt, char *s) {
-  void *Tmb = T(mb);
-  D(O(Tcr, Tmb, Tmb, Tmb, Tmb, Tmb, Tmb, Tmb,
-      O(Tcr,
-        T(t[-7], terminal_ray),
-        T(t[-6], terminal_ray),
-        T(t[-5], terminal_ray),
-        T(t[-4], terminal_ray),
-        T(t[-3], terminal_ray),
-        T(t[-2], terminal_ray),
-        T(t[-1], terminal_ray),
-        O(Tcr, Tmb, Tmb, Tmb, Tmb, Tmb, Tmb, Tmb, o))),
+  D(O(Tcr, Tpr, Tpr, Tpr, Tpr, Tpr, Tpr, Tpr,
+      O(Tcr, T(t[-7], terminal_ray), T(t[-6], terminal_ray),
+        T(t[-5], terminal_ray), T(t[-4], terminal_ray), T(t[-3], terminal_ray),
+        T(t[-2], terminal_ray), T(t[-1], terminal_ray), o)),
     t - 7, s);
 }
 void dec17(obrt, rest) {
-  D(o, T(t, continues, "1", "2", "3", "4", "5", "6", "7", terminal), frwd);
+  t = T(t, continues, "1", "2", "3", "4", "5", "6", "7", terminal);
+  D(o, t, frwd);
 }
-void E(obrt, char *s) {
-  void *Tmb = T(mb);
-  D(O(Tcr, Tmb, Tmb, Tmb, Tmb, Tmb, Tmb, Tmb,
+void alfa(obrt, rest) {
+  t = T(t, continues, "a", "b", "c", "d", "e", "f", "g", terminal);
+  D(o, t, frwd);
+}
+// sS = (term_s ‘thenS‘ sS ‘thenS‘ sS) ‘orelse‘ empty
+
+void sS(obrt, rest) {
+  D(O(Tcr, Tpr, Tpr, Tpr, Tpr, Tpr, Tpr, Tpr,
+      O(Tcr, T("s", "s", "s", "s", "s", "s", "s", terminal, sS, sS), T(cr),
+        T(cr), T(cr), T(cr), T(cr), T(cr), o)),
+    t, frwd);
+}
+void dec17s(obrt, rest) {
+  D(O(Tcr, Tpr, Tpr, Tpr, Tpr, Tpr, Tpr, Tpr,
       O(Tcr,
-        T(mb, "1", term),
-        T(mb, "2", term),
-        T(mb, "(", term, E, ")", term),
-        T(mb, E, "*", term, E),
-        T(mb, E, "/", term, E),
-        T(mb, E, "+", term, E),
-        T(mb, E, "-", term, E),
-        O(Tcr, Tmb, Tmb, Tmb, Tmb, Tmb, Tmb, Tmb, o))),
-    t, s);
+        T(alfa),
+        T(dec17s, dec17),
+        T(dec17s, alfa),
+        T(cr),
+        T(cr),
+        T(cr),
+        T(cr), o)),
+    t, frwd);
 }
+void reduce(obrt, rest) { D(O(T(0), o), t, frwd); }
+// void E(obrt, char *s) {
+//   void *Tmb = T(mb);
+//   D(O(Tcr, Tmb, Tmb, Tmb, Tmb, Tmb, Tmb, Tmb,
+//       O(Tcr, T("1", term, reduce), T("2", term, reduce),
+//         T("(", term, E, ")", term, reduce), T(E, "*", term, E, reduce),
+//         T(E, "/", term, E, reduce), T(E, "+", term, E, reduce),
+//         T(E, "-", term, E, reduce),
+//         O(Tcr, Tmb, Tmb, Tmb, Tmb, Tmb, Tmb, Tmb, o))),
+//     t, s);
+// }
 void logger();
 void wrap_logger();
-void show_rez(obrt, char *s) { printf("%s\n", s); }
-void call_back(obrt, char *s) {
-  show_rez(o, b, r, t, s);
-  if ((long)o == (long)b[8])
-    return;
+void show_rez(obrt, char *s) {
+  printf("%s\n", s);
   usleep(200000);
-  D(b, t, s);
 }
 int main() {
   long b = 0, r = 4;
-  void (**t)() = T(mb);
-  void *o[] = {T(show_rez), 0, 0, 0, T(call_back), 0, 0, 0, 0};
-  D(o, t, "baaaa");
+  void (**t)() = T(mb, dec17s);
+  void *Tshow = T(show_rez);
+  void *o[] = {Tshow, Tshow, Tshow, Tshow, Tshow, Tshow, Tshow, Tshow, 0};
+  D(o, t, "a123123");
 }
