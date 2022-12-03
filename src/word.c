7d09d127e2c12eb6991812b62cb1b63159f55c33
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-12-03 16:05:28 +0400

    wait

--- src/word.c
+++ src/word.c
@@ -1,10 +1,11 @@
 #define Nargs(...) (sizeof((const void *[]){__VA_ARGS__}) / sizeof(void *))
 #define D(o, t, ...) t[-1](o, b, r, t - 1, __VA_ARGS__)
-#define O(r0, r1, r2, r3, o, b) ((void *)(const void *[]){r0, r1, r2, r3, o, b})
+#define O(r0, r1, r2, r3, o) ((void *)(const void *[]){r0, r1, r2, r3, o})
 #define T(...)                                                                 \
   ((void *)&((const void *[]){                                                 \
       cr, __VA_ARGS__, Nargs(cr, __VA_ARGS__)})[Nargs(cr, __VA_ARGS__)])
-typedef void (*w_t)(void *, void *, void *, void *, void *, void *, void *, void *);
+typedef void (*w_t)(void *, void *, void *, void *, void *, void *, void *,
+                    void *);
 #define obrtd w_t **o, w_t **b, long r, w_t *t, long d
 
 #define rest void *p1, void *p2, void *p3
@@ -25,30 +26,44 @@ void ε() {}
 void propeller(obrtd, char *s, long l, long p) {
   if (d < (l - p) + 5)
     for (r = 0; r < 4; r++)
-      D(o, t, d - 1, s, l, p);
+      D(O(T(cr), T(cr), T(cr), T(cr), o), t, d - 1, s, l, p);
 }
-#define V(ar, T0, T1, T2, T3)                                                  \
-  void ar(obrtd, char *s, long l, long p) {                             \
+// clang-format off
+#define V(ar, T0, R0, T1, R1, T2, R2, T3, R3)                                                  \
+  void ar(obrtd, char *s, long l, long p) {                                    \
     void *Tpro = T(propeller);                                                 \
-    o = O(Tpro, Tpro, Tpro, Tpro, O(T0, T1, T2, T3, o, "."), #ar);             \
+    o = O(Tpro, Tpro, Tpro, Tpro,                                              \
+          O(T0,   T1,   T2,   T3, o));                                         \
     D(o, t, d + 1, s, l, p);                                                   \
   }
-V(dot, T(".", dim), T(ε), T(ε), T(ε))
+void *Trid=0,*Trcat=0;
+V(dot,  T(".", dim),  T(ε), T(ε), T(ε), T(ε), T(ε), T(ε), T(ε))
 
-V(α, T("a", dim), T(ε), T(ε), T(ε))
-V(β, T("b", dim), T(ε), T(ε), T(ε))
-V(χ, T("x", dim), T(ε), T(ε), T(ε))
-V(αβ, T(α), T(β), T(ε), T(ε))
-V(S, T(β), T(S, α), T(S, β), T(S, χ))
+V(plus, T("+", dim),  T(ε), T(ε), T(ε), T(ε), T(ε), T(ε), T(ε))
+V(mul,  T("*", dim),  T(ε), T(ε), T(ε), T(ε), T(ε), T(ε), T(ε))
+V(oprn, T("(", dim),  T(ε), T(ε), T(ε), T(ε), T(ε), T(ε), T(ε))
+V(cprn, T(")", dim),  T(ε), T(ε), T(ε), T(ε), T(ε), T(ε), T(ε))
+void *ratoi;
+V(Nat,  T("0", dim), T(ratoi),
+        T("1", dim), T(ratoi),
+        T("2", dim), T(ratoi),
+        T("3", dim), T(ratoi))
+V(E,    T(Nat),             T(ε),
+        T(oprn, E, cprn),   T(ε),
+        T(E, mul, E),       T(ε),
+        T(E, plus, E),      T(ε));
+// o b r t s
+V(α,  T("a", dim), T(ε), T(ε), T(ε), T(ε), T(ε), T(ε), T(ε))
+V(β,  T("b", dim), T(ε), T(ε), T(ε), T(ε), T(ε), T(ε), T(ε))
+V(χ,  T("x", dim), T(ε), T(ε), T(ε), T(ε), T(ε), T(ε), T(ε))
+V(S,  T(β),     Trid, 
+      T(S, α),  Trcat,
+      T(S, β),  Trcat,
+      T(S, χ),  Trcat)
 
-V(σ, T("s", dim), T(ε), T(ε), T(ε))
-V(sS, T(σ, sS, sS), T(cr), T(ε), T(ε))
-
-V(plus, T("+", dim), T(ε), T(ε), T(ε))
-V(mul, T("*", dim), T(ε), T(ε), T(ε))
-V(oprn, T("(", dim), T(ε), T(ε), T(ε))
-V(cprn, T(")", dim), T(ε), T(ε), T(ε))
-V(E, T(α), T(oprn, E, cprn), T(E, mul, E), T(E, plus, E))
+// V(σ, T("s", dim), T(ε), T(ε), T(ε))
+// V(sS, T(σ, sS, sS), T(cr), T(ε), T(ε))
+// 
 // s ::= np vp | s pp
 // np ::= noun | det noun | np pp
 // pp ::= prep np
@@ -57,15 +72,15 @@ V(E, T(α), T(oprn, E, cprn), T(E, mul, E), T(E, plus, E))
 // noun ::= ’i’ | ’m’ | ’p’ | ’b’
 // verb ::= ’s’
 // prep ::= ’n’ | ’w’
-V(prep, T("n", dim), T("w", dim), T(ε), T(ε))
-V(verb, T("s", dim), T(ε), T(ε), T(ε))
-V(noun, T("i", dim), T("m", dim), T("p", dim), T("b", dim))
-V(det, T("a", dim), T("t", dim), T(ε), T(ε))
-void pp();
-V(np, T(noun), T(det, noun), T(np, pp), T(ε))
-V(vp, T(verb, np), T(ε), T(ε), T(ε))
-V(pp, T(prep, np), T(ε), T(ε), T(ε))
-V(s, T(np, vp), T(s, pp), T(ε), T(ε))
+// V(prep, T("n", dim), T("w", dim), T(ε), T(ε))
+// V(verb, T("s", dim), T(ε), T(ε), T(ε))
+// V(noun, T("i", dim), T("m", dim), T("p", dim), T("b", dim))
+// V(det, T("a", dim), T("t", dim), T(ε), T(ε))
+// void pp();
+// V(np, T(noun), T(det, noun), T(np, pp), T(ε))
+// V(vp, T(verb, np), T(ε), T(ε), T(ε))
+// V(pp, T(prep, np), T(ε), T(ε), T(ε))
+// V(s, T(np, vp), T(s, pp), T(ε), T(ε))
 #include <string.h>
 void zero(obrtd, char *s, long l, long p) {
   printf("%ld %ld %s %ld/%ld\n", r, d, s, l, p);
@@ -73,14 +88,14 @@ void zero(obrtd, char *s, long l, long p) {
 }
 int main() {
   void *T0 = T(zero); //, *Tε = T(ε);
-  void *o = O(T0, T0, T0, T0, 0, "zero");
+  void *o = O(T0, T0, T0, T0, 0);
   // char *str = "isamntpwab.";
   // w_t *t = T(s, dot);
   // char *str = "bababx.";
   // w_t *t = T(S,dot);
-  char *str = "ssss";
-  w_t *t = T(sS);
-  // char *str = "(a+a)*a*(a+a).";
-  // w_t *t = T(E, dot);
+  // char *str = "ssss";
+  // w_t *t = T(sS);
+  char *str = "(1+(1+(1+(1+2)*2+1)*2+1)*2+1)*2+1.";
+  w_t *t = T(E, dot);
   t[-1](o, 0, 0, t - 1, 0, str, strlen(str), 0);
 }
