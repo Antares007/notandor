d848c70811ffc554ca1b2f16af52ade7b5124208
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-12-05 04:01:44 +0400

    use single parameter for state

--- src/word.c
+++ src/word.c
@@ -1,12 +1,12 @@
 #define Nargs(...) (sizeof((const void *[]){__VA_ARGS__}) / sizeof(void *))
 #define D(o, t, ...) t[-1](o, b, r, t - 1, __VA_ARGS__)
 #define O(r0, r1, r2, r3, o) ((void *)(const void *[]){r0, r1, r2, r3, o})
+#define A(...) ((void *[]){__VA_ARGS__})
 #define T(...)                                                                 \
   ((void *)&((const void *[]){                                                 \
       cr, __VA_ARGS__, Nargs(cr, __VA_ARGS__)})[Nargs(cr, __VA_ARGS__)])
-typedef void (*w_t)(void *, void *, void *, void *, void *, void *, void *,
-                    void *);
-#define obrtd w_t **o, w_t **b, long r, w_t *t, long d
+typedef void (*w_t)(void *, void *, void *, void *, void *, void *);
+#define obrtds w_t **o, w_t **b, long r, w_t *t, long d, void **s
 
 #define rest void *p1, void *p2, void *p3
 #define frwd p1, p2, p3
@@ -15,42 +15,47 @@ typedef void (*w_t)(void *, void *, void *, void *, void *, void *, void *,
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-void cr(obrtd, rest) { o[r][-1](o[4], b, r, o[r] - 1, d, frwd); }
-void dim(obrtd, char *s, long l, long p) {
-  char *m = (char *)*--t;
-  if (p < l && m[0] == s[p])
-    D(o, t, d, s, l, p + 1);
+void cr(obrtds) { o[r][-1](o[4], b, r, o[r] - 1, d, s); }
+void dim(obrtds) {
+  char *in = s[0];
+  long len = s[1];
+  long pos = s[2];
+  char *str = (char *)*--t;
+  if (pos < len && str[0] == in[pos])
+    D(o, t, d, A(in, len, pos + 1));
 }
 void ε() {}
-void propeller(obrtd, char *s, long l, long p) {
-  if (d < (l - p) + 5)
+void propeller(obrtds) {
+  long len = s[1];
+  long pos = s[2];
+  if (d < (len - pos) + 5)
     for (r = 0; r < 4; r++)
-      D(O(T(cr), T(cr), T(cr), T(cr), o), t, d - 1, s, l, p);
+      D(O(T(cr), T(cr), T(cr), T(cr), o), t, d - 1, s);
 }
-void print_o_name(obrtd, char *s, long l, long p) {
+void print_o_name(obrtds) {
   printf("%s\n", (char *)*--t);
-  D(o, t, d, s, l, p);
+  D(o, t, d, s);
 }
-void br(obrtd, char *s, long l, long p) {
+void br(obrtds) {
   b = O(T(propeller), T(propeller), T(propeller), T(propeller),
         O(T(ε), T(ε), T(ε), T(ε), b));
   b[4][r] = T(*--t, print_o_name);
-  D(o, t, d, s, l, p);
+  D(o, t, d, s);
 }
-void term(obrtd, rest) {
+void term(obrtds) {
   void *str = *--t;
   void *Tbr = T(str, br);
   o = O(T(propeller), T(propeller), T(propeller), T(propeller),
         O(T(str, dim), T(ε), T(ε), T(ε), O(Tbr, Tbr, Tbr, Tbr, o)));
-  D(o, t, d + 1, frwd);
+  D(o, t, d + 1, s);
 }
 #define V(ar, T0, T1, T2, T3)                                                  \
-  void ar(obrtd, char *s, long l, long p) {                                    \
+  void ar(obrtds) {                                                            \
     void *Tpro = T(propeller);                                                 \
     void *Tbr = T(#ar, br);                                                    \
     o = O(Tpro, Tpro, Tpro, Tpro,                                              \
           O(Tbr, Tbr, Tbr, Tbr, O(T0, T1, T2, T3, o)));                        \
-    D(o, t, d + 1, s, l, p);                                                   \
+    D(o, t, d + 1, s);                                                         \
   }
 void *Trid = 0, *Trcat = 0;
 V(dot, T(".", dim), T(ε), T(ε), T(ε))
@@ -85,13 +90,13 @@ V(sS, T("s", term, sS, sS), T(cr), T(ε), T(ε))
 // V(pp, T(prep, np), T(ε), T(ε), T(ε))
 // V(s, T(np, vp), T(s, pp), T(ε), T(ε))
 #include <string.h>
-void zero(obrtd, char *s, long l, long p) {
-  printf("%ld %ld %s %ld/%ld\n", r, d, s, l, p);
-  cr(b, 0, 0, 0, 0, s, l, p);
+void zero(obrtds) {
+  printf("%ld %ld %s %ld/%ld\n", r, d, (char *)s[0], (long)s[1], (long)s[2]);
+  cr(b, 0, 0, 0, 0, s);
   usleep(10000);
 }
-void one(obrtd, char *s, long l, long p) {
-  printf("%s\n", s);
+void one(obrtds) {
+  printf("one\n");
   usleep(10000);
 }
 int main() {
@@ -101,11 +106,11 @@ int main() {
   void *b = O(T1, T1, T1, T1, 1);
   // char *str = "isamntpwab.";
   // w_t *t = T(s, dot);
-  char *str = "babaax.";
-  w_t *t = T(S, dot);
+  // char *str = "babaax.";
+  // w_t *t = T(S, dot);
   // char *str = "ss";
   // w_t *t = T(sS);
   // char *str = "(1+(1+(1+(1+2)*2+1)*2+1)*2+1)*2+1.";
   // w_t *t = T(E, dot);
-  t[-1](o, b, 0, t - 1, 0, str, strlen(str), 0);
+  t[-1](o, b, 0, t - 1, 0, A(str, strlen(str), 0));
 }
