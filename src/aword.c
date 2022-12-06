67ad85cdc6b89d269c8d06bc621c8a0f325af39c
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-12-06 12:41:06 +0400

    reorg fun

--- src/aword.c
+++ src/aword.c
@@ -39,6 +39,21 @@ N(propeller) {
   for (r = 0; r < 4; r++)
     t[a - 1].c(o, b, a - 1, r, t, s);
 }
+N(wrap) {
+  s_t *To = t[--a].s;
+  s_t *Tb = t[--a].s;
+  t[a - 1].c(O(Tb, Tb, Tb, Tb, O(To, To, To, To, o)), b, a - 1, r, t, s);
+}
+void word(obarts, void *wordcore) {
+  s_t *Tp = T(propeller, wordcore);
+  t[a - 1].c(O(T(Tp, T(cr0), wrap),     //
+               T(Tp, T(cr1), wrap),     //
+               T(Tp, T(cr2), wrap),     //
+               T(Tp, T(cr3), wrap), o), //
+             b, a - 1, r, t, s);
+}
+#include <stdio.h>
+#include <string.h>
 N(match) {
   const char *in = s[1].cs;
   long len = s[2].q;
@@ -47,19 +62,6 @@ N(match) {
   if (pos < len && str[0] == in[pos])
     t[a - 1].c(o, b, a - 1, r, t, O(s, in, (void *)len, (void *)(pos + 1)));
 }
-N(wrap) {
-  s_t *To = t[--a].s;
-  s_t *Tb = t[--a].s;
-  t[a - 1].c(O(Tb, Tb, Tb, Tb, O(To, To, To, To, o)), b, a - 1, r, t, s);
-}
-void word(obarts, void *w) {
-  s_t *Tp = T(propeller, w);
-  t[a - 1].c(O(T(Tp, T(cr0), wrap), T(Tp, T(cr1), wrap), T(Tp, T(cr2), wrap),
-               T(Tp, T(cr3), wrap), o),
-             b, a - 1, r, t, s);
-}
-#include <stdio.h>
-#include <string.h>
 N(dig47_o) {
   t[a - 1].c(O(T("4", match), T("5", match), T("6", match), T("7", match), o),
              b, a - 1, r, t, s);
@@ -71,21 +73,16 @@ N(dig03_o) {
 }
 N(dig03) { word(o, b, a, r, t, s, dig03_o); }
 N(Nat_o) {
-  t[a - 1].c(O(T(dig03), T(dig47), T("8", match), T("9", match), o),
-             b, a - 1, r, t, s);
+  t[a - 1].c(O(T(dig03), T(dig47), T("8", match), T("9", match), o), b, a - 1,
+             r, t, s);
 }
-N(Nat) {
-  word(o, b, a, r, t, s, Nat_o);
-}
+N(Nat) { word(o, b, a, r, t, s, Nat_o); }
 N(ε) {}
 N(Nats);
 N(Nats_o) {
-  t[a - 1].c(O(T(Nat), T(Nats, Nat), T(ε), T(ε), o),
-             b, a - 1, r, t, s);
+  t[a - 1].c(O(T(Nat), T(Nats, Nat), T(ε), T(ε), o), b, a - 1, r, t, s);
 }
-N(Nats) {
-  word(o, b, a, r, t, s, Nats_o);
-}
+N(Nats) { word(o, b, a, r, t, s, Nats_o); }
 
 N(operator_o) {
   t[a - 1].c(O(T("+", match), T("-", match), T("*", match), T("/", match), o),
@@ -97,8 +94,9 @@ N(end_o) { printf("end_o %ld %s %ld %ld\n", r, s[1].cs, s[2].q, s[3].q); }
 N(end_b) { printf("end_b\n"); }
 int main() {
   s_t *t = T(Nats);
-  char*str = "0987654321";
+  char *str = "0987654321";
   t[t[-1].q - 1].c(O(T(end_o), T(end_o), T(end_o), T(end_o), 0), //
                    O(T(end_b), T(end_b), T(end_b), T(end_b), 0), //
-                   t[-1].q - 1, 1, t, O(0, str, (void *)strlen(str), (void *)0));
+                   t[-1].q - 1, 1, t,
+                   O(0, str, (void *)strlen(str), (void *)0));
 }
