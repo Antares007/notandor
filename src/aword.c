fa3fe54e3f1ebfab8faeb70eea9901f10fde7713
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-12-08 14:23:11 +0400

    memoize to save stack space. need better solution.

--- src/aword.c
+++ src/aword.c
@@ -4,8 +4,8 @@
   ((s_t *)&(const void *[]){(void *)Nargs(__VA_ARGS__), __VA_ARGS__}[1])
 #define O(...) ((s_t *)(const void *[]){__VA_ARGS__})
 
-#define D(o)                                                                   \
-  logn(o, b, a, r, t, s, __FUNCTION__), t[a - 1].c(o, b, a - 1, r, t, s)
+#define D_(o) t[a - 1].c(o, b, a - 1, r, t, s)
+#define D(o) logn(o, b, a, r, t, s, __FUNCTION__), D_(o)
 
 #define obarts                                                                 \
   struct s_t *o, struct s_t *b, long a, long r, struct s_t *t, struct s_t *s
@@ -29,7 +29,7 @@ void logn(obarts, const char *name) {
   printf("%ld ", r);
   for (s_t *p = o; p; p = p[4].s)
     printf("  ");
-  printf("%s %ld\n", name, s[3].q), usleep(100000);
+  printf("%s\n", name), usleep(1000000);
 }
 // memoize what and when?
 // we need to remove cycle from propeller and find perfect cycle!
@@ -70,22 +70,24 @@ N(match) {
 }
 #define W(a, b) T(bo, a, b, wrap_1x1)
 N(dign_o) {
-  b = o = O(T(bo, "0", match), T(bo, "1", match), T(bo, "2", match),
-            T(bo, "3", match), O(T(bo), T(bo), T(bo), T(bo), o));
-  D(o);
+  D(O(T(bo, "0", match), T(bo, "1", match), T(bo, "2", match),
+      T(bo, "3", match), o));
 }
-N(lend) {
-  //  printf(">%ld\n", s[3].q);
-  bo(b, b, 0, (r + 1) % 4, 0, s);
+
+N(rec) { D(b); }
+N(the_word) {
+  if (o[5].c == the_word)
+    D_(o);
+  else
+    D_(O(T(bo), T(bo), T(bo), T(bo), o, the_word));
 }
 void perfect_loop() {
-  s_t *o = O(T(bo, "0", match), T(bo, "1", match), T(bo, "2", match),
-             T(bo, "3", match),
-             O(T(bo), T(bo), T(bo), T(bo),
-               O(T(bo, lend), T(bo, lend), T(bo, lend), T(bo, lend), 0)));
-  s_t *s = O(0, "333", (void *)3, (void *)0);
-  bo(o, o, 0, 0, 0, s);
+  s_t *o = O(
+      T(bo, the_word), T(bo, the_word), T(bo, the_word), T(bo, the_word),
+      O(T(bo, rec, cr1), T(bo, rec, cr2), T(bo, rec, cr3), T(bo, rec, cr0), 0));
+  bo(o, o, 0, 0, 0, 0);
 }
+
 N(end_o) { printf("end_o %ld %s %ld %ld\n", r, s[1].cs, s[2].q, s[3].q); }
 N(end_b) { printf("end_b\n"); }
 int main() {
