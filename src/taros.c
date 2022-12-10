b30adb0def58d32818a36164eb774a57ad4dd285
Archil Bolkvadze <a.bolkvadze@gmail.com>
2022-12-10 09:13:24 +0400

    extract taros.h

--- src/taros.c
+++ src/taros.c
@@ -1,28 +1,9 @@
-#define NofArgs(...)                                                           \
-  (sizeof((const void *[]){__VA_ARGS__}) / sizeof(const void *))
-#define LOG (void)0
-#define D(o) LOG, t[a - 1].c(t, a - 1, r, o, s)
-#define B(r0, r1, r2, r3, oa, ob, oc, od)                                      \
-  T(r0, r1, r2, r3, (void *)oa, (void *)ob, (void *)oc, (void *)od)
-#define T(...)                                                                 \
-  ((taros *)&(void *[]){(void *)NofArgs(__VA_ARGS__), __VA_ARGS__}[1])
-#define N(argos)                                                               \
-  void argos(struct taros *t, long a, long r, struct taros *o, long s)
-typedef struct taros {
-  union {
-    struct taros *o;
-    signed long q;
-    void *v;
-    const char *cs;
-    N((*c));
-  };
-} taros;
+#include "taros.h"
 #include <stdio.h>
 #include <unistd.h>
+#include <assert.h>
 #undef LOG
 #define LOG printf("%ld/%ld %s\n", s, r, __FUNCTION__), usleep(100000)
-
-#include <assert.h>
 N(cr) {
   assert(a == 0);
   t = o[r].o;
---
+++ src/taros.h
@@ -0,0 +1,20 @@
+#pragma once
+#define NofArgs(...)                                                           \
+  (sizeof((const void *[]){__VA_ARGS__}) / sizeof(const void *))
+#define LOG (void)0
+#define D(o) LOG, t[a - 1].c(t, a - 1, r, o, s)
+#define B(r0, r1, r2, r3, oa, ob, oc, od)                                      \
+  T(r0, r1, r2, r3, (void *)oa, (void *)ob, (void *)oc, (void *)od)
+#define T(...)                                                                 \
+  ((taros *)&(void *[]){(void *)NofArgs(__VA_ARGS__), __VA_ARGS__}[1])
+#define N(argos)                                                               \
+  void argos(struct taros *t, long a, long r, struct taros *o, long s)
+typedef struct taros {
+  union {
+    struct taros *o;
+    signed long q;
+    void *v;
+    const char *cs;
+    N((*c));
+  };
+} taros;
