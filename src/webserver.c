39c991af485db329ecabcc45b411c7b74a881e3b
Archil Bolkvadze <a.bolkvadze@gmail.com>
2023-01-21 09:29:06 +0400

    add description

--- src/webserver.c
+++ src/webserver.c
@@ -1,3 +1,41 @@
+/*
+This code is a basic implementation of an HTTP server. It uses the C standard
+
+library for socket programming to create and bind a socket, listen for incoming
+connections, and accept connections.
+
+The first group of includes are the headers that the program needs to use socket
+programming, in addition to the standard input/output library and the string
+library.
+
+The error() function is a simple function that prints an error message to the
+console and exits the program.
+
+The send_response() function takes in a socket descriptor, a status code, and a
+body, and sends an HTTP response with that status code and body over the socket.
+
+The handle_get_request() function takes in a socket descriptor and a resource
+(the requested file path), opens the file at that path, reads it into a buffer,
+and sends the contents of the file as the body of an HTTP response with a status
+code of 200 OK. If the file is not found, it sends a response with a status code
+of 404 Not Found.
+
+The handle_post_request() function takes in a socket descriptor and a resource
+(which is not used in this example), reads incoming data from the socket, and
+sends an HTTP response with a status code of 200 OK and the message "Data
+received" as the body.
+
+The main() function sets up the socket, binds it to a port, and enters a loop
+that waits for incoming connections. When a connection is accepted, it reads the
+incoming HTTP request, parses the method and resource from it, and calls the
+appropriate handling function (handle_get_request() or handle_post_request())
+based on the method.
+
+Overall, the code is a basic implementation of an HTTP server which can handle
+GET and POST request. It's missing some important feature like request
+validation, thread handling, and security, but it demonstrates the core
+functionality of an HTTP server.
+*/
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
